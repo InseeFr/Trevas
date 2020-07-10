99b9c2ac231b1d87fc1a2bab64b5663f11e298db
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-07-10 11:22:05 +0200

    Make parser and model dependencies transitive

--- java-vtl-engine/src/main/java/module-info.java
+++ java-vtl-engine/src/main/java/module-info.java
@@ -4,8 +4,8 @@ import javax.script.ScriptEngineFactory;
 
 module fr.insee.vtl.engine {
     requires java.scripting;
-    requires fr.insee.vtl.parser;
-    requires fr.insee.vtl.model;
+    requires transitive fr.insee.vtl.parser;
+    requires transitive fr.insee.vtl.model;
     requires org.antlr.antlr4.runtime;
     provides ScriptEngineFactory with VtlScriptEngineFactory;
 }
