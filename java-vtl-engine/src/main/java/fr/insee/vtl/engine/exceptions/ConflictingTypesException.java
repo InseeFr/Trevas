2989fd0aee2131cf78fb1cdbd0109e129db66698
Franck <franck.cotton@insee.fr>
2020-07-10 16:53:27 +0200

    First implementation of conflicting type exception

--- java-vtl-engine/src/main/java/fr/insee/vtl/engine/exceptions/ConflictingTypesException.java
+++ java-vtl-engine/src/main/java/fr/insee/vtl/engine/exceptions/ConflictingTypesException.java
@@ -1,12 +1,12 @@
 package fr.insee.vtl.engine.exceptions;
 
-import org.antlr.v4.runtime.tree.ParseTree;
-
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
-public class ConflictingTypeException extends VtlScriptException {
-    public ConflictingTypeException(ParseTree tree, List<Class<?>> types) {
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class ConflictingTypesException extends VtlScriptException {
+    public ConflictingTypesException(ParseTree tree, Collection<Class<?>> types) {
         super(String.format("conflicting types: %s", types.stream().map(Class::getSimpleName)
                 .collect(Collectors.toList())), tree);
     }
