c18e1016e30632f72bbbb1693175c17d6e6f8823
Franck Cotton <franck.cotton@insee.fr>
2020-07-21 09:20:00 +0200

    Javadoc (#25)
    * JavaDoc for package fr.insee.vtl.engine.exceptions
    
    * Additional JavaDoc for the engine

--- vtl-engine/src/main/java/fr/insee/vtl/engine/VtlScriptEngine.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/VtlScriptEngine.java
@@ -13,14 +13,30 @@ import javax.script.*;
 import java.io.IOException;
 import java.io.Reader;
 
+/**
+ * <code>VtlScriptEngine</code> provides base methods for the VTL script engine.
+ */
 public class VtlScriptEngine extends AbstractScriptEngine {
 
     private final ScriptEngineFactory factory;
 
+    /**
+     * Constructor taking a script engine factory.
+     *
+     * @param factory The script engine factory associated to the script engine to create.
+     */
     public VtlScriptEngine(ScriptEngineFactory factory) {
         this.factory = factory;
     }
 
+    /**
+     * Base method for the evaluation of a script expression in a given context.
+     *
+     * @param stream The script to evaluate represented as a stream of Unicode code points.
+     * @param context The evaluation context (for example: data bindings).
+     * @return The result of the evaluation of the script in the given context.
+     * @throws VtlScriptException In case of error during the evaluation.
+     */
     private Object evalStream(CodePointCharStream stream, ScriptContext context) throws VtlScriptException {
         try {
             VtlLexer lexer = new VtlLexer(stream);
@@ -37,12 +53,28 @@ public class VtlScriptEngine extends AbstractScriptEngine {
         }
     }
 
+    /**
+     * Evaluation of a script expression (represented as a string) in a given context.
+     *
+     * @param script The script to evaluate represented as a string.
+     * @param context The evaluation context (for example: data bindings).
+     * @return The result of the evaluation of the script in the given context.
+     * @throws VtlScriptException In case of error during the evaluation.
+     */
     @Override
     public Object eval(String script, ScriptContext context) throws VtlScriptException {
         CodePointCharStream stream = CharStreams.fromString(script);
         return evalStream(stream, context);
     }
 
+    /**
+     * Evaluation of a script expression (read in a <code>Reader</code>) in a given context.
+     *
+     * @param reader The <code>Reader</code> containing the script to evaluate.
+     * @param context The evaluation context (for example: data bindings).
+     * @return The result of the evaluation of the script in the given context.
+     * @throws ScriptException
+     */
     @Override
     public Object eval(Reader reader, ScriptContext context) throws ScriptException {
         try {
@@ -53,11 +85,21 @@ public class VtlScriptEngine extends AbstractScriptEngine {
         }
     }
 
+    /**
+     * Returns an new instance of script context bindings.
+     *
+     * @return A new instance of bindings (<code>SimpleBindings</code> object).
+     */
     @Override
     public Bindings createBindings() {
         return new SimpleBindings();
     }
 
+    /**
+     * Returns the script engine factory associated to the script engine.
+     *
+     * @return The script engine factory associated to the script engine.
+     */
     @Override
     public ScriptEngineFactory getFactory() {
         return factory;
--- vtl-engine/src/main/java/fr/insee/vtl/engine/VtlScriptEngineFactory.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/VtlScriptEngineFactory.java
@@ -4,63 +4,126 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
 import java.util.List;
 
+/**
+ * <code>VtlScriptEngineFactory</code> is used to instantiate VTL script engines and hold their basic description.
+ */
 public class VtlScriptEngineFactory implements ScriptEngineFactory {
 
+    /**
+     * Returns the full  name of the VTL script engine.
+     *
+     * @return The full  name of the VTL script engine.
+     */
     @Override
     public String getEngineName() {
         return "Java VTL engine";
     }
 
+    /**
+     * Returns the version number of the VTL script engine.
+     *
+     * @return The version number of the VTL script engine.
+     */
     @Override
     public String getEngineVersion() {
         return "0.1.0";
     }
 
+    /**
+     * Returns a list of filename extensions associated to VTL scripts.
+     *
+     * @return The list of filename extensions associated to VTL scripts.
+     */
     @Override
     public List<String> getExtensions() {
         return List.of("vtl");
     }
 
+    /**
+     * Returns a list of MIME types corresponding to VTL scripts.
+     *
+     * @return The list of MIME types corresponding to VTL scripts (empty list for now).
+     */
     @Override
     public List<String> getMimeTypes() {
         return List.of();
     }
 
+    /**
+     * Returns a list of short names for the VTL script engine.
+     *
+     * @return A list of short names for the VTL script engine.
+     */
     @Override
     public List<String> getNames() {
         return List.of(getLanguageName());
     }
 
+    /**
+     * Returns the name of the scripting language supported by the engine.
+     *
+     * @return The name of the scripting language supported by the engine ("VTL").
+     */
     @Override
     public String getLanguageName() {
         return "vtl";
     }
 
+    /**
+     * Returns the version of the scripting language supported by the engine.
+     *
+     * @return The version of the scripting language supported by the engine ("2.1").
+     */
     @Override
     public String getLanguageVersion() {
         return "2.1";
     }
 
+    /**
+     * The <code>getParameter</code> method is not supported by this engine factory.
+     *
+     * @throws An empty <code>UnsupportedOperationException</code>.
+     */
     @Override
     public Object getParameter(String key) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * The <code>getMethodCallSyntax</code> method is not supported by this engine factory.
+     *
+     * @throws An empty <code>UnsupportedOperationException</code>.
+     */
     @Override
     public String getMethodCallSyntax(String obj, String m, String... args) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * The <code>getOutputStatement</code> method is not supported by this engine factory.
+     *
+     * @throws An empty <code>UnsupportedOperationException</code>.
+     */
     @Override
     public String getOutputStatement(String toDisplay) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * The <code>getProgram</code> method is not supported by this engine factory.
+     *
+     * @throws An empty <code>UnsupportedOperationException</code>.
+     */
     @Override
     public String getProgram(String... statements) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns a VTL script engine instance associated with this factory.
+
+     * @return A new <code>VtlScriptEngine</code> instance.
+     */
     @Override
     public ScriptEngine getScriptEngine() {
         return new VtlScriptEngine(this);
