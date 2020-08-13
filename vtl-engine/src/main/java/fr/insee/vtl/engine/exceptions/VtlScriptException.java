46f6d07537e84008363075a3a5b939e95c49c1e9
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-08-13 10:52:55 +0200

    Add position of the tree to the VtlScriptException

--- vtl-engine/src/main/java/fr/insee/vtl/engine/exceptions/VtlScriptException.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/exceptions/VtlScriptException.java
@@ -1,13 +1,15 @@
 package fr.insee.vtl.engine.exceptions;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import javax.script.ScriptException;
 
 /**
  * The <code>VtlScriptException</code> is the base class for all VTL exceptions.
  */
-// TODO: Implement file, line & getters.
 public class VtlScriptException extends ScriptException {
 
     private final ParseTree tree;
@@ -26,12 +28,24 @@ public class VtlScriptException extends ScriptException {
     /**
      * Constructor taking the mother exception and the parsing context.
      *
-     * @param e The mother exception.
+     * @param e    The mother exception.
      * @param tree The parsing context where the exception is thrown.
      */
     public VtlScriptException(Exception e, ParseTree tree) {
         super(e);
         this.tree = tree;
+    }
+
+    public static Position positionOf(ParseTree tree) {
+        if (tree instanceof ParserRuleContext) {
+            ParserRuleContext parserRuleContext = (ParserRuleContext) tree;
+            return new Position(parserRuleContext.getStart(), parserRuleContext.getStop());
+        }
+        if (tree instanceof TerminalNode) {
+            var terminalNode = ((TerminalNode) tree);
+            return new Position(terminalNode.getSymbol());
+        }
+        throw new IllegalStateException();
     }
 
     /**
@@ -41,5 +55,48 @@ public class VtlScriptException extends ScriptException {
      */
     public ParseTree getTree() {
         return tree;
+    }
+
+    public Position getPosition() {
+        return positionOf(getTree());
+    }
+
+    public static class Position {
+
+        private final Integer startLine;
+        private final Integer endLine;
+        private final Integer startColumn;
+        private final Integer endColumn;
+
+
+        private Position(Token from, Token to) {
+            this.startLine = from.getLine() - 1;
+            this.endLine = to.getLine() - 1;
+            this.startColumn = from.getCharPositionInLine();
+            this.endColumn = to.getCharPositionInLine() + (to.getStopIndex() - to.getStartIndex() + 1);
+        }
+
+        private Position(Token token) {
+            this.startLine = token.getLine() - 1;
+            this.endLine = token.getLine() - 1;
+            this.startColumn = token.getCharPositionInLine();
+            this.endColumn = token.getCharPositionInLine() + (token.getStopIndex() - token.getStartIndex() + 1);
+        }
+
+        public Integer getStartLine() {
+            return startLine;
+        }
+
+        public Integer getEndLine() {
+            return endLine;
+        }
+
+        public Integer getStartColumn() {
+            return startColumn;
+        }
+
+        public Integer getEndColumn() {
+            return endColumn;
+        }
     }
 }
--- vtl-engine/src/test/java/fr/insee/vtl/engine/VtlScriptEngineTest.java
+++ vtl-engine/src/test/java/fr/insee/vtl/engine/VtlScriptEngineTest.java
@@ -1,6 +1,9 @@
 package fr.insee.vtl.engine;
 
+import fr.insee.vtl.engine.exceptions.InvalidTypeException;
 import fr.insee.vtl.engine.exceptions.UndefinedVariableException;
+import fr.insee.vtl.engine.exceptions.VtlScriptException;
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,11 +21,36 @@ public class VtlScriptEngineTest {
         engine = new ScriptEngineManager().getEngineByName("vtl");
     }
 
+    public static <T extends Throwable> Condition<T> atPosition(Integer line, Integer startColumn, Integer endColumn) {
+        return atPosition(line, line, startColumn, endColumn);
+    }
+
+    public static <T extends Throwable> Condition<T> atPosition(Integer startLine, Integer endLine,
+                                                                Integer startColumn, Integer endColumn) {
+        return new Condition<>(throwable -> {
+            var scriptException = (VtlScriptException) throwable;
+            var position = scriptException.getPosition();
+            return position.getStartLine().equals(startLine) &&
+                    position.getEndLine().equals(endLine) &&
+                    position.getStartColumn().equals(startColumn) &&
+                    position.getEndColumn().equals(endColumn);
+        }, "at position <%d,%d;%d,%d>",
+                startLine, endLine, startColumn, endColumn);
+    }
+
     @Test
     public void testExceptions() {
         assertThatThrownBy(() -> {
             engine.eval("var := undefinedVariable + 42;");
         }).isInstanceOf(UndefinedVariableException.class)
+                .is(atPosition(0, 7, 24))
                 .hasMessage("undefined variable undefinedVariable");
+
+        assertThatThrownBy(() -> {
+            engine.eval("var := true and (10 +\n" +
+                    "10);");
+        }).isInstanceOf(InvalidTypeException.class)
+                .is(atPosition(0, 1, 16, 3))
+                .hasMessage("invalid type Long, expected (10+10) to be Boolean");
     }
 }
