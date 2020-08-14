88a22b319cc3cde2a3a21e4b7765953aee2254f3
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-08-14 12:09:43 +0200

    Implement calc clause

--- vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/ClauseVisitor.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/ClauseVisitor.java
@@ -8,9 +8,7 @@ import fr.insee.vtl.model.ResolvableExpression;
 import fr.insee.vtl.parser.VtlBaseVisitor;
 import fr.insee.vtl.parser.VtlParser;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ClauseVisitor extends VtlBaseVisitor<DatasetExpression> {
@@ -24,6 +22,47 @@ public class ClauseVisitor extends VtlBaseVisitor<DatasetExpression> {
         Map<String, Object> componentMap = datasetExpression.getDataStructure().stream()
                 .collect(Collectors.toMap(Dataset.Component::getName, component -> component));
         this.componentExpressionVisitor = new ExpressionVisitor(componentMap);
+    }
+
+    @Override
+    public DatasetExpression visitCalcClause(VtlParser.CalcClauseContext ctx) {
+
+        var structure = new ArrayList<>(datasetExpression.getDataStructure());
+        var expressions = new HashMap<String, ResolvableExpression>();
+        for (VtlParser.CalcClauseItemContext calcCtx : ctx.calcClauseItem()) {
+
+            // TODO: Should be an expression so we can handle membership better.
+            var columnName = calcCtx.componentID().getText();
+            ResolvableExpression calc = componentExpressionVisitor.visit(calcCtx);
+
+            // We construct a new structure
+            // TODO: Handle role. Ie: Optional.ofNullable(calcCtx.componentRole());
+            structure.add(new Dataset.Component(columnName, calc.getType(), Dataset.Role.MEASURE));
+
+            expressions.put(columnName, calc);
+        }
+
+        return new DatasetExpression() {
+            @Override
+            public Dataset resolve(Map<String, Object> context) {
+                var dataset = datasetExpression.resolve(context);
+                var columns = getColumnNames();
+                List<List<Object>> result = dataset.getDataAsMap().stream().map(map -> {
+                    var newMap = new HashMap<>(map);
+                    for (String columnName : expressions.keySet()) {
+                        newMap.put(columnName, expressions.get(columnName).resolve(newMap));
+                    }
+                    return newMap;
+                }).map(map -> Dataset.mapToRowMajor(map, columns)).collect(Collectors.toList());
+                return new InMemoryDataset(result, structure);
+            }
+
+            @Override
+            public List<Dataset.Component> getDataStructure() {
+                return structure;
+            }
+        };
+
     }
 
     @Override
--- vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/ClauseVisitorTest.java
+++ vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/ClauseVisitorTest.java
@@ -29,8 +29,9 @@ public class ClauseVisitorTest {
 
         InMemoryDataset dataset = new InMemoryDataset(
                 List.of(
-                        Map.of("name", "Hadrien", "age", 10L, "weight", 10L),
-                        Map.of("name", "Nico", "age", 11L, "weight", 10L)
+                        Map.of("name", "Hadrien", "age", 10L, "weight", 11L),
+                        Map.of("name", "Nico", "age", 11L, "weight", 10L),
+                        Map.of("name", "Franck", "age", 12L, "weight", 9L)
                 ),
                 Map.of("name", String.class, "age", Long.class, "weight", Long.class),
                 Map.of("name", Role.IDENTIFIER, "age", Role.MEASURE, "weight", Role.MEASURE)
@@ -39,12 +40,39 @@ public class ClauseVisitorTest {
         ScriptContext context = engine.getContext();
         context.setAttribute("ds1", dataset, ScriptContext.ENGINE_SCOPE);
 
-        engine.eval("ds := ds1[filter age = weight];");
+        engine.eval("ds := ds1[filter age > 10 and age < 12];");
 
         assertThat(engine.getContext().getAttribute("ds")).isInstanceOf(Dataset.class);
         assertThat(((Dataset) engine.getContext().getAttribute("ds")).getDataAsMap()).isEqualTo(List.of(
-                Map.of("name", "Hadrien", "age", 10L, "weight", 10L)
+                Map.of("name", "Nico", "age", 11L, "weight", 10L)
         ));
+
+    }
+
+    @Test
+    public void testCalcClause() throws ScriptException {
+
+        InMemoryDataset dataset = new InMemoryDataset(
+                List.of(
+                        Map.of("name", "Hadrien", "age", 10L, "weight", 11L),
+                        Map.of("name", "Nico", "age", 11L, "weight", 10L),
+                        Map.of("name", "Franck", "age", 12L, "weight", 9L)
+                ),
+                Map.of("name", String.class, "age", Long.class, "weight", Long.class),
+                Map.of("name", Role.IDENTIFIER, "age", Role.MEASURE, "weight", Role.MEASURE)
+        );
+
+        ScriptContext context = engine.getContext();
+        context.setAttribute("ds1", dataset, ScriptContext.ENGINE_SCOPE);
+
+        engine.eval("ds := ds1[calc res := age + weight / 2];");
+
+        assertThat(engine.getContext().getAttribute("ds")).isInstanceOf(Dataset.class);
+        assertThat(((Dataset) engine.getContext().getAttribute("ds")).getDataAsMap()).containsExactly(
+                Map.of("name", "Hadrien", "res", 15.5, "age", 10L, "weight", 11L),
+                Map.of("name", "Nico", "res", 16.0, "age", 11L, "weight", 10L),
+                Map.of("name", "Franck", "res", 16.5, "age", 12L, "weight", 9L)
+        );
 
     }
 }
