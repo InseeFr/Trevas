b8ccddc366c8774e9dcf56439f1a47af6c37668e
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-08-21 14:23:12 +0200

    Support dataPoints and dataStructure fields

--- vtl-jackson/src/main/java/fr/insee/vtl/jackson/DatasetDeserializer.java
+++ vtl-jackson/src/main/java/fr/insee/vtl/jackson/DatasetDeserializer.java
@@ -7,8 +7,12 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import fr.insee.vtl.model.Dataset;
 import fr.insee.vtl.model.InMemoryDataset;
 
+import javax.script.Bindings;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DatasetDeserializer extends StdDeserializer<Dataset> {
 
@@ -18,19 +22,31 @@ public class DatasetDeserializer extends StdDeserializer<Dataset> {
 
     @Override
     public Dataset deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        // TODO: Refactor in methods that check names.
-        // Advance the parser to structure value.
-        p.nextFieldName();
-        p.nextToken();
-        var listOfComponentType = ctxt.getTypeFactory().constructCollectionLikeType(List.class, Dataset.Component.class);
-        List<Dataset.Component> listOfComponents = ctxt.readValue(p, listOfComponentType);
 
+        // Json is an object.
+        var token = p.currentToken();
+        if (!token.isStructStart()) {
+            ctxt.handleUnexpectedToken(Bindings.class, p);
+        }
 
-        p.nextFieldName();
-        p.nextToken();
-        var listOfListType = ctxt.getTypeFactory().constructCollectionLikeType(List.class, List.class);
-        List<List<Object>> data = ctxt.readValue(p, listOfListType);
+        Map<String, Object> values = new HashMap<>();
+        if (Set.of("structure", "dataStructure").contains(p.nextFieldName())) {
+            p.nextToken();
+            var listOfComponentType = ctxt.getTypeFactory().constructCollectionLikeType(List.class, Dataset.Component.class);
+            List<Dataset.Component> listOfComponents = ctxt.readValue(p, listOfComponentType);
+            values.put("structure", listOfComponents);
+        }
 
-        return new InMemoryDataset(data, listOfComponents);
+        if (Set.of("data", "dataPoints").contains(p.nextFieldName())) {
+            p.nextToken();
+            var listOfListType = ctxt.getTypeFactory().constructCollectionLikeType(List.class, List.class);
+            List<List<Object>> data = ctxt.readValue(p, listOfListType);
+            values.put("data", data);
+        }
+
+        return new InMemoryDataset(
+                (List<List<Object>>) values.get("data"),
+                (List<Dataset.Component>) values.get("structure")
+        );
     }
 }
