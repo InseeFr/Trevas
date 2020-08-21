967e4a6199e2b8952530c341d86cadd9379dcb09
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-08-21 13:48:55 +0200

    Implement BindingsDeserializer

--- vtl-jackson/src/main/java/fr/insee/vtl/jackson/BindingsDeserializer.java
+++ vtl-jackson/src/main/java/fr/insee/vtl/jackson/BindingsDeserializer.java
@@ -2,10 +2,13 @@ package fr.insee.vtl.jackson;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import fr.insee.vtl.model.Dataset;
 
 import javax.script.Bindings;
+import javax.script.SimpleBindings;
 import java.io.IOException;
 
 public class BindingsDeserializer extends StdDeserializer<Bindings> {
@@ -16,6 +19,25 @@ public class BindingsDeserializer extends StdDeserializer<Bindings> {
 
     @Override
     public Bindings deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        return null;
+
+        var bindings = new SimpleBindings();
+        var token = p.currentToken();
+        if (!token.isStructStart()) {
+            ctxt.handleUnexpectedToken(Bindings.class, p);
+        }
+
+        while (p.nextToken() == JsonToken.FIELD_NAME) {
+            var name = p.currentName();
+            var value = p.nextValue();
+            if (value.isStructStart()) {
+                // Dataset
+                bindings.put(name, ctxt.readValue(p, Dataset.class));
+            } else {
+                // Scalar
+                bindings.put(name, ctxt.readValue(p, Object.class));
+            }
+        }
+
+        return bindings;
     }
 }
