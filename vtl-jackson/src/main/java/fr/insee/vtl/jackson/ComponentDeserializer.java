724958ec32d56679d7639fbc5ac52557f4f96960
NicoLaval <nicolas.laval@insee.fr>
2020-09-02 08:45:18 +0200

    Fix minor issues

--- vtl-jackson/src/main/java/fr/insee/vtl/jackson/ComponentDeserializer.java
+++ vtl-jackson/src/main/java/fr/insee/vtl/jackson/ComponentDeserializer.java
@@ -1,7 +1,6 @@
 package fr.insee.vtl.jackson;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import fr.insee.vtl.model.Dataset;
