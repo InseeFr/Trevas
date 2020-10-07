a1881204426873e3f8955072f1ccfb8d83e53e26
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-10-07 10:46:52 +0200

    Add tests for serializers

--- vtl-jackson/src/test/java/fr/insee/vtl/jackson/AbstractMapperTest.java
+++ vtl-jackson/src/test/java/fr/insee/vtl/jackson/AbstractMapperTest.java
@@ -3,12 +3,14 @@ package fr.insee.vtl.jackson;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.IOException;
+
 public class AbstractMapperTest {
 
     protected ObjectMapper mapper;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         mapper = new ObjectMapper();
         mapper.registerModule(new TrevasModule());
     }
