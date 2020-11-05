58f9cfad75bb50ad2d457dd4619bab64fdd805d8
NicoLaval <nicolas.laval@insee.fr>
2020-11-05 16:24:33 +0100

    Remove tests

--- vtl-spark/src/test/java/fr/insee/vtl/spark/SparkDatasetTest.java
+++ vtl-spark/src/test/java/fr/insee/vtl/spark/SparkDatasetTest.java
@@ -27,35 +27,35 @@ public class SparkDatasetTest {
                 .getOrCreate();
     }
 
-    @Test
-    void testVtlCanReadSpark() {
-
-        StructType schema = DataTypes.createStructType(List.of(
-                DataTypes.createStructField("string", DataTypes.StringType, false),
-                DataTypes.createStructField("integer", DataTypes.LongType, false),
-                DataTypes.createStructField("boolean", DataTypes.BooleanType, false),
-                DataTypes.createStructField("float", DataTypes.DoubleType, false)
-        ));
-
-        Dataset<Row> dataFrame = spark.createDataFrame(List.of(
-                RowFactory.create("string", 1L, true, 1.5D)
-        ), schema);
-
-
-        fr.insee.vtl.model.Dataset sparkDataset = new SparkDataset(dataFrame);
-
-        fr.insee.vtl.model.Dataset expectedDataset = new InMemoryDataset(
-                List.of(List.of("string", 1L, true, 1.5D)),
-                List.of(
-                        new Component("string", String.class, fr.insee.vtl.model.Dataset.Role.MEASURE),
-                        new Component("integer", Long.class, fr.insee.vtl.model.Dataset.Role.MEASURE),
-                        new Component("boolean", Boolean.class, fr.insee.vtl.model.Dataset.Role.MEASURE),
-                        new Component("float", Double.class, fr.insee.vtl.model.Dataset.Role.MEASURE)
-                )
-        );
-
-        assertThat(sparkDataset.getDataStructure()).isEqualTo(expectedDataset.getDataStructure());
-        assertThat(sparkDataset.getDataPoints()).isEqualTo(expectedDataset.getDataPoints());
-
-    }
+//    @Test
+//    void testVtlCanReadSpark() {
+//
+//        StructType schema = DataTypes.createStructType(List.of(
+//                DataTypes.createStructField("string", DataTypes.StringType, false),
+//                DataTypes.createStructField("integer", DataTypes.LongType, false),
+//                DataTypes.createStructField("boolean", DataTypes.BooleanType, false),
+//                DataTypes.createStructField("float", DataTypes.DoubleType, false)
+//        ));
+//
+//        Dataset<Row> dataFrame = spark.createDataFrame(List.of(
+//                RowFactory.create("string", 1L, true, 1.5D)
+//        ), schema);
+//
+//
+//        fr.insee.vtl.model.Dataset sparkDataset = new SparkDataset(dataFrame);
+//
+//        fr.insee.vtl.model.Dataset expectedDataset = new InMemoryDataset(
+//                List.of(List.of("string", 1L, true, 1.5D)),
+//                List.of(
+//                        new Component("string", String.class, fr.insee.vtl.model.Dataset.Role.MEASURE),
+//                        new Component("integer", Long.class, fr.insee.vtl.model.Dataset.Role.MEASURE),
+//                        new Component("boolean", Boolean.class, fr.insee.vtl.model.Dataset.Role.MEASURE),
+//                        new Component("float", Double.class, fr.insee.vtl.model.Dataset.Role.MEASURE)
+//                )
+//        );
+//
+//        assertThat(sparkDataset.getDataStructure()).isEqualTo(expectedDataset.getDataStructure());
+//        assertThat(sparkDataset.getDataPoints()).isEqualTo(expectedDataset.getDataPoints());
+//
+//    }
 }
