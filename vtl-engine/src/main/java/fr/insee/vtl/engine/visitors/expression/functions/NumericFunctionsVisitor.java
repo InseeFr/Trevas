5873608815c60cdb82ddac0e4107b6d5e4c607cd
NicoLaval <nicolas.laval@insee.fr>
2020-11-08 17:43:06 +0100

    Add power & log support

--- vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/expression/functions/NumericFunctionsVisitor.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/expression/functions/NumericFunctionsVisitor.java
@@ -36,6 +36,12 @@ public class NumericFunctionsVisitor extends VtlBaseVisitor<ResolvableExpression
             case VtlParser.MOD:
                 // TODO: Support dataset.
                 return handleModulo(ctx.left, ctx.right);
+            case VtlParser.POWER:
+                // TODO: Support dataset.
+                return handlePower(ctx.left, ctx.right);
+            case VtlParser.LOG:
+                // TODO: Support dataset.
+                return handleLog(ctx.left, ctx.right);
             default:
                 throw new UnsupportedOperationException("unknown operator " + ctx);
         }
@@ -51,6 +57,33 @@ public class NumericFunctionsVisitor extends VtlBaseVisitor<ResolvableExpression
             Double leftDouble = leftValue instanceof Long ? ((Long) leftValue).doubleValue() : (Double) leftValue;
             Double rightDouble = rightValue instanceof Long ? ((Long) rightValue).doubleValue() : (Double) rightValue;
             return rightDouble.equals(0D) ? leftDouble : leftDouble % rightDouble;
+        });
+    }
+
+    private ResolvableExpression handlePower(VtlParser.ExprContext left, VtlParser.ExprContext right) {
+        var leftExpression = assertNumber(exprVisitor.visit(left), left);
+        var rightExpression = assertNumber(exprVisitor.visit(right), right);
+        return DoubleExpression.of(context -> {
+            var leftValue = leftExpression.resolve(context);
+            var rightValue = rightExpression.resolve(context);
+            Double leftDouble = leftValue instanceof Long ? ((Long) leftValue).doubleValue() : (Double) leftValue;
+            Double rightDouble = rightValue instanceof Long ? ((Long) rightValue).doubleValue() : (Double) rightValue;
+            return Math.pow(leftDouble, rightDouble);
+        });
+    }
+
+    private ResolvableExpression handleLog(VtlParser.ExprContext left, VtlParser.ExprContext right) {
+        var leftExpression = assertNumber(exprVisitor.visit(left), left);
+        var baseExpression = assertNumber(exprVisitor.visit(right), right);
+        return DoubleExpression.of(context -> {
+            var leftValue = leftExpression.resolve(context);
+            var baseValue = baseExpression.resolve(context);
+            Double leftDouble = leftValue instanceof Long ? ((Long) leftValue).doubleValue() : (Double) leftValue;
+            Double baseDouble = baseValue instanceof Long ? ((Long) baseValue).doubleValue() : (Double) baseValue;
+            //TODO:
+            // If leftDouble <= 0
+            // If baseDouble <= 1
+            return Math.log(leftDouble) / Math.log(baseDouble);
         });
     }
 
--- vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/expression/functions/NumericFunctionsTest.java
+++ vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/expression/functions/NumericFunctionsTest.java
@@ -33,11 +33,57 @@ public class NumericFunctionsTest {
         engine.eval("d := mod(9, 0);");
         assertThat(context.getAttribute("d")).isEqualTo(9D);
         assertThatThrownBy(() -> {
-            engine.eval("d := mod(10, \"ko\");");
+            engine.eval("e := mod(10, \"ko\");");
         }).isInstanceOf(InvalidTypeException.class)
                 .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
         assertThatThrownBy(() -> {
-            engine.eval("e := mod(\"ko\", 10);");
+            engine.eval("f := mod(\"ko\", 10);");
+        }).isInstanceOf(InvalidTypeException.class)
+                .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
+    }
+
+    @Test
+    public void testPower() throws ScriptException {
+        ScriptContext context = engine.getContext();
+        engine.eval("a := power(5, 2);");
+        assertThat(context.getAttribute("a")).isEqualTo(25D);
+        engine.eval("b := power(5, 1);");
+        assertThat(context.getAttribute("b")).isEqualTo(5D);
+        engine.eval("c := power(5, 0);");
+        assertThat(context.getAttribute("c")).isEqualTo(1D);
+        engine.eval("d := power(5, -1);");
+        assertThat(context.getAttribute("d")).isEqualTo(0.2D);
+        engine.eval("e := power(-5, 3);");
+        assertThat(context.getAttribute("e")).isEqualTo(-125D);
+        assertThatThrownBy(() -> {
+            engine.eval("f := power(10, \"ko\");");
+        }).isInstanceOf(InvalidTypeException.class)
+                .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
+        assertThatThrownBy(() -> {
+            engine.eval("e := power(\"ko\", 10);");
+        }).isInstanceOf(InvalidTypeException.class)
+                .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
+    }
+
+    @Test
+    public void testLog() throws ScriptException {
+        ScriptContext context = engine.getContext();
+        engine.eval("a := log(1024, 2);");
+        assertThat(context.getAttribute("a")).isEqualTo(10D);
+//        assertThatThrownBy(() -> {
+//            engine.eval("b := log(1024, 1);");
+//        }).isInstanceOf(InvalidTypeException.class)
+//                .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
+//        assertThatThrownBy(() -> {
+//            engine.eval("c := log(-2, 10);");
+//        }).isInstanceOf(InvalidTypeException.class)
+//                .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
+        assertThatThrownBy(() -> {
+            engine.eval("d := log(10, \"ko\");");
+        }).isInstanceOf(InvalidTypeException.class)
+                .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
+        assertThatThrownBy(() -> {
+            engine.eval("e := log(\"ko\", 10);");
         }).isInstanceOf(InvalidTypeException.class)
                 .hasMessage("invalid type String, expected \"ko\" to be Double or Long");
     }
