ac35dd2d5634a3e3a6a1d32625a8331ea21f443b
NicoLaval <nicolas.laval@insee.fr>
2020-11-30 10:47:29 +0100

    Fix import issue

--- vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/expression/functions/SetFunctionsVisitor.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/expression/functions/SetFunctionsVisitor.java
@@ -10,10 +10,8 @@ import org.antlr.v4.runtime.RuleContext;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static fr.insee.vtl.engine.utils.TypeChecking.assertTypeExpression;
 
