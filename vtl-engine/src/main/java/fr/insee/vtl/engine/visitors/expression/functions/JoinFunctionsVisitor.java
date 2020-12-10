b66a6c582767961fd1c436246b740d15f1005dbf
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-12-10 10:23:45 +0100

    Add tests and improve left join

--- vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/expression/functions/JoinFunctionsVisitor.java
+++ vtl-engine/src/main/java/fr/insee/vtl/engine/visitors/expression/functions/JoinFunctionsVisitor.java
@@ -1,11 +1,21 @@
 package fr.insee.vtl.engine.visitors.expression.functions;
 
+import fr.insee.vtl.engine.exceptions.InvalidArgumentException;
+import fr.insee.vtl.engine.exceptions.VtlRuntimeException;
 import fr.insee.vtl.engine.visitors.expression.ExpressionVisitor;
+import fr.insee.vtl.model.Dataset;
 import fr.insee.vtl.model.DatasetExpression;
+import fr.insee.vtl.model.InMemoryDataset;
+import fr.insee.vtl.model.Structured;
 import fr.insee.vtl.parser.VtlBaseVisitor;
 import fr.insee.vtl.parser.VtlParser;
+import org.antlr.v4.runtime.RuleContext;
 
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static fr.insee.vtl.engine.utils.TypeChecking.assertTypeExpression;
+import static fr.insee.vtl.model.Structured.*;
 
 public class JoinFunctionsVisitor extends VtlBaseVisitor<DatasetExpression> {
 
@@ -29,8 +39,112 @@ public class JoinFunctionsVisitor extends VtlBaseVisitor<DatasetExpression> {
         throw new UnsupportedOperationException("unknown join type");
     }
 
+    /**
+     * Normalize the
+     *
+     * @param joinClauseContext
+     * @return
+     */
+    private LinkedHashMap<String, DatasetExpression> normalizeDatasets(VtlParser.JoinClauseContext joinClauseContext) {
+        LinkedHashMap<String, DatasetExpression> datasets = new LinkedHashMap<>();
+        for (VtlParser.JoinClauseItemContext joinClauseItem : joinClauseContext.joinClauseItem()) {
+            // Expression here is not so nice if alias is optional.. We force var id.
+            var datasetExpressionContext = joinClauseItem.expr();
+            if (!(datasetExpressionContext instanceof VtlParser.VarIdExprContext)) {
+                throw new VtlRuntimeException(
+                        new InvalidArgumentException("use a variable", datasetExpressionContext));
+            }
+            var alias = Optional.<RuleContext>ofNullable(joinClauseItem.alias())
+                    .orElse(datasetExpressionContext).getText();
+            var datasetExpression = (DatasetExpression) assertTypeExpression(
+                    expressionVisitor.visit(datasetExpressionContext),
+                    Dataset.class, datasetExpressionContext);
+            datasets.put(alias, datasetExpression);
+        }
+        return datasets;
+    }
+
     private DatasetExpression leftJoin(VtlParser.JoinExprContext ctx) {
-        throw new UnsupportedOperationException("TODO: left_join");
+        var joinClauseContext = ctx.joinClause();
+        var datasets = normalizeDatasets(joinClauseContext);
+
+        // TODO: support "using"
+        // Build a list of common identifier (type and name) in all the datasets
+        var identfiers = datasets.values().stream()
+                .map(Structured::getDataStructure)
+                .flatMap(structure -> structure.values().stream())
+                .filter(component -> Dataset.Role.IDENTIFIER.equals(component.getRole()))
+                .distinct()
+                .collect(Collectors.toList());
+
+        if (identfiers.isEmpty()) {
+            throw new VtlRuntimeException(
+                    new InvalidArgumentException("datasets must have common identifiers", joinClauseContext)
+            );
+        }
+
+        var iterator = datasets.values().iterator();
+        DatasetExpression result = handleLeftJoin(identfiers, iterator.next(), iterator.next());
+        return result;
+    }
+
+    private DatasetExpression handleLeftJoin(List<Component> identifiers, DatasetExpression left, DatasetExpression right) {
+        // Create common structure
+        List<Component> components = new ArrayList<>(identifiers);
+        for (Component component : left.getDataStructure().values()) {
+            if (!identifiers.contains(component)) {
+                components.add(component);
+            }
+        }
+        for (Component component : right.getDataStructure().values()) {
+            if (!identifiers.contains(component)) {
+                components.add(component);
+            }
+        }
+
+        var structure = new DataStructure(components);
+
+        // Predicate for the join. Could be using the
+        Comparator<DataPoint> predicate = (dl, dr) -> {
+            for (Component identifier : identifiers) {
+                if (!Objects.equals(dl.get(identifier.getName()), dr.get(identifier.getName()))) {
+                    return -1;
+                }
+            }
+            return 0;
+        };
+
+        return new DatasetExpression() {
+            @Override
+            public Dataset resolve(Map<String, Object> context) {
+                var leftPoints = left.resolve(context).getDataPoints();
+                var rightPoints = right.resolve(context).getDataPoints();
+                List<DataPoint> result = new ArrayList<>();
+                for (DataPoint leftPoint : leftPoints) {
+                    // Create merge datapoint.
+                    var mergedPoint = new DataPoint(structure);
+                    for (String leftColumn : left.getDataStructure().keySet()) {
+                        mergedPoint.set(leftColumn, leftPoint.get(leftColumn));
+                    }
+
+                    for (DataPoint rightPoint : rightPoints) {
+                        // Check equality
+                        if (predicate.compare(leftPoint, rightPoint) == 0) {
+                            for (String rightColumn : right.getDataStructure().keySet()) {
+                                mergedPoint.set(rightColumn, rightPoint.get(rightColumn));
+                            }
+                        }
+                    }
+                    result.add(mergedPoint);
+                }
+                return new InMemoryDataset(result, structure);
+            }
+
+            @Override
+            public DataStructure getDataStructure() {
+                return structure;
+            }
+        };
     }
 
     private DatasetExpression crossJoin(VtlParser.JoinExprContext ctx) {
--- vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/expression/functions/JoinFunctionsTest.java
+++ vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/expression/functions/JoinFunctionsTest.java
@@ -1,11 +1,19 @@
 package fr.insee.vtl.engine.visitors.expression.functions;
 
+import fr.insee.vtl.model.Dataset;
+import fr.insee.vtl.model.InMemoryDataset;
+import fr.insee.vtl.model.Structured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.List;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class JoinFunctionsTest {
@@ -17,25 +25,110 @@ public class JoinFunctionsTest {
     }
 
     @Test
-    void testLeftJoin() {
+    public void testJoinWithAlias() throws ScriptException {
+
+        InMemoryDataset dataset1 = new InMemoryDataset(
+                List.of(
+                        List.of("a", 1L, 2L),
+                        List.of("b", 3L, 4L),
+                        List.of("c", 5L, 6L)
+                ),
+                List.of(
+                        new Structured.Component("name", String.class, Dataset.Role.IDENTIFIER),
+                        new Structured.Component("age", Long.class, Dataset.Role.MEASURE),
+                        new Structured.Component("weight", Long.class, Dataset.Role.MEASURE)
+                )
+        );
+        InMemoryDataset dataset2 = new InMemoryDataset(
+                List.of(
+                        List.of(7L, "a", 8L),
+                        List.of(9L, "b", 10L),
+                        List.of(11L, "c", 12)
+                ),
+                List.of(
+                        new Structured.Component("age2", Long.class, Dataset.Role.MEASURE),
+                        new Structured.Component("name", String.class, Dataset.Role.IDENTIFIER),
+                        new Structured.Component("weight2", Long.class, Dataset.Role.MEASURE)
+                )
+        );
+        InMemoryDataset dataset3 = new InMemoryDataset(
+                List.of(),
+                List.of(
+                        new Structured.Component("name", String.class, Dataset.Role.IDENTIFIER),
+                        new Structured.Component("age3", Long.class, Dataset.Role.MEASURE),
+                        new Structured.Component("weight3", Long.class, Dataset.Role.MEASURE)
+                )
+        );
+
+        ScriptContext context = engine.getContext();
+        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds1", dataset1);
+        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds2", dataset2);
+        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds3", dataset3);
+
+        engine.eval("result := left_join(ds1 as dsOne, ds2, ds3)");
+    }
+
+    @Test
+    public void testLeftJoin() {
         assertThatThrownBy(() -> engine.eval("result := left_join(a, b, c)"))
                 .hasMessage("TODO: left_join");
     }
 
     @Test
-    void testInnerJoin() {
+    public void testLeftJoinWithDouble() throws ScriptException {
+        InMemoryDataset dataset1 = new InMemoryDataset(
+                List.of(
+                        List.of("Hadrien", 10L),
+                        List.of("Nico", 11L)
+                ),
+                List.of(
+                        new Structured.Component("name", String.class, Dataset.Role.IDENTIFIER),
+                        new Structured.Component("age", Long.class, Dataset.Role.MEASURE)
+                )
+        );
+        InMemoryDataset dataset2 = new InMemoryDataset(
+                List.of(
+                        List.of("Hadrien", 20L),
+                        List.of("Nico", 22L)
+                ),
+                List.of(
+                        new Structured.Component("name", String.class, Dataset.Role.IDENTIFIER),
+                        new Structured.Component("age", Long.class, Dataset.Role.MEASURE)
+                )
+        );
+
+        ScriptContext context = engine.getContext();
+        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds1", dataset1);
+        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds2", dataset2);
+
+        engine.eval("result := left_join(ds1, ds2)");
+        assertThat(((Dataset) engine.getContext().getAttribute("result")).getDataAsMap()).containsExactlyInAnyOrder(
+                Map.of("pseudo", "Hadrien", "age", 11L),
+                Map.of("pseudo", "Nico", "age", 11L)
+        );
+
+        engine.eval("result := left_join(ds1, ds2 apply ds1 + ds2)");
+        assertThat(((Dataset) engine.getContext().getAttribute("result")).getDataAsMap()).containsExactlyInAnyOrder(
+                Map.of("pseudo", "Hadrien", "age", 30L),
+                Map.of("pseudo", "Nico", "age", 30L)
+        );
+
+    }
+
+    @Test
+    public void testInnerJoin() {
         assertThatThrownBy(() -> engine.eval("result := inner_join(a, b, c)"))
                 .hasMessage("TODO: inner_join");
     }
 
     @Test
-    void testCrossJoin() {
+    public void testCrossJoin() {
         assertThatThrownBy(() -> engine.eval("result := cross_join(a, b, c)"))
                 .hasMessage("TODO: cross_join");
     }
 
     @Test
-    void testFullJoin() {
+    public void testFullJoin() {
         assertThatThrownBy(() -> engine.eval("result := full_join(a, b, c)"))
                 .hasMessage("TODO: full_join");
     }
