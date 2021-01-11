6de4bb5124106d2863891cf04d934550f3b30a0c
Hadrien Kohl <hadrien.kohl@gmail.com>
2021-01-11 10:50:13 +0100

    Add support for duplicate component in left join

--- vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/expression/functions/JoinFunctionsTest.java
+++ vtl-engine/src/test/java/fr/insee/vtl/engine/visitors/expression/functions/JoinFunctionsTest.java
@@ -27,32 +27,6 @@ public class JoinFunctionsTest {
     }
 
     @Test
-    public void testDuplicateNameNotSupportedYet() {
-        InMemoryDataset dataset1 = new InMemoryDataset(
-                List.of(),
-                List.of(
-                        new Structured.Component("name", String.class, Role.IDENTIFIER),
-                        new Structured.Component("age", Long.class, Role.MEASURE),
-                        new Structured.Component("weight", Long.class, Role.MEASURE)
-                )
-        );
-        InMemoryDataset dataset2 = new InMemoryDataset(
-                List.of(),
-                List.of(
-                        new Structured.Component("name", String.class, Role.IDENTIFIER),
-                        new Structured.Component("age", Long.class, Role.MEASURE),
-                        new Structured.Component("weight", Long.class, Role.MEASURE)
-                )
-        );
-
-        engine.getContext().setAttribute("ds1", dataset1, ScriptContext.ENGINE_SCOPE);
-        engine.getContext().setAttribute("ds2", dataset2, ScriptContext.ENGINE_SCOPE);
-        assertThatThrownBy(() -> engine.eval("result := left_join(ds1 as dsOne, ds2);"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("duplicate");
-    }
-
-    @Test
     public void testLeftJoinWithUsing() throws ScriptException {
         var ds1 = new InMemoryDataset(
                 List.of(
@@ -88,78 +62,25 @@ public class JoinFunctionsTest {
 
         engine.getContext().setAttribute("ds1", ds1, ScriptContext.ENGINE_SCOPE);
         engine.getContext().setAttribute("ds2", ds2, ScriptContext.ENGINE_SCOPE);
-        engine.eval("result := left_join(ds1, ds2 using id1);");
+        engine.eval("result := left_join(ds1, ds2 as aliasDs using id1);");
 
         var result = (Dataset) engine.getContext().getAttribute("result");
+        assertThat(result.getColumnNames()).containsExactlyInAnyOrder(
+                "id1", "ds1#id2", "m1", "aliasDs#id2", "m2"
+        );
         assertThat(result.getDataAsList()).containsExactlyInAnyOrder(
-                Arrays.asList("a", 1L, 2L, 9L, 10L, 16L, 17L),
-                Arrays.asList("b", 3L, 4L, 11L, 12L, 18L, 19L),
-                Arrays.asList("c", 5L, 6L, 12L, 13L, 20L, 21L),
-                Arrays.asList("c", 5L, 6L, 12L, 13L, 22L, 23L),
-                Arrays.asList("c", 5L, 6L, 14L, 15L, 20L, 21L),
-                Arrays.asList("c", 5L, 6L, 14L, 15L, 22L, 23L),
-                Arrays.asList("d", 7L, 8L, null, null, null, null)
-        );
-    }
-
-    @Test
-    public void testJoinWithAlias() throws ScriptException {
-
-        InMemoryDataset dataset1 = new InMemoryDataset(
-                List.of(
-                        List.of("a", 1L, 2L),
-                        List.of("b", 3L, 4L),
-                        List.of("c", 5L, 6L),
-                        List.of("d", 7L, 8L)
-                ),
-                List.of(
-                        new Structured.Component("name", String.class, Role.IDENTIFIER),
-                        new Structured.Component("age", Long.class, Role.MEASURE),
-                        new Structured.Component("weight", Long.class, Role.MEASURE)
-                )
-        );
-        InMemoryDataset dataset2 = new InMemoryDataset(
-                List.of(
-                        List.of(9L, "a", 10L),
-                        List.of(11L, "b", 12L),
-                        List.of(12L, "c", 13L),
-                        List.of(14L, "c", 15L)
-                ),
-                List.of(
-                        new Structured.Component("age2", Long.class, Role.MEASURE),
-                        new Structured.Component("name", String.class, Role.IDENTIFIER),
-                        new Structured.Component("weight2", Long.class, Role.MEASURE)
-                )
-        );
-
-        InMemoryDataset dataset3 = new InMemoryDataset(
-                List.of(
-                        List.of(16L, "a", 17L),
-                        List.of(18L, "b", 19L),
-                        List.of(20L, "c", 21L),
-                        List.of(22L, "c", 23L)
-                ),
-                List.of(
-                        new Structured.Component("age3", Long.class, Role.MEASURE),
-                        new Structured.Component("name", String.class, Role.IDENTIFIER),
-                        new Structured.Component("weight3", Long.class, Role.MEASURE)
-                )
-        );
-
-        ScriptContext context = engine.getContext();
-        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds1", dataset1);
-        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds2", dataset2);
-        context.getBindings(ScriptContext.ENGINE_SCOPE).put("ds3", dataset3);
-
-        engine.eval("result := left_join(ds1 as dsOne, ds2, ds3);");
-
-        var result = (Dataset) context.getAttribute("result");
-        assertThat(result.getDataAsList()).containsExactlyInAnyOrder(
-                Arrays.asList("a", 1L, 2L, 9L, 10L),
-                Arrays.asList("b", 3L, 4L, 11L, 12L),
-                Arrays.asList("c", 5L, 6L, 12L, 13L),
-                Arrays.asList("c", 5L, 6L, 14L, 15L),
-                Arrays.asList("d", 7L, 8L, null, null)
+                 Arrays.asList("a", 1L, 1L, 1L, 7L),
+                 Arrays.asList("a", 1L, 1L, 2L, 8L),
+                 Arrays.asList("a", 2L, 2L, 1L, 7L),
+                 Arrays.asList("a", 2L, 2L, 2L, 8L),
+                 Arrays.asList("b", 1L, 3L, 1L, 9L),
+                 Arrays.asList("b", 1L, 3L, 2L, 10L),
+                 Arrays.asList("b", 2L, 4L, 1L, 9L),
+                 Arrays.asList("b", 2L, 4L, 2L, 10L),
+                 Arrays.asList("c", 1L, 5L, 3L, 11L),
+                 Arrays.asList("c", 1L, 5L, 4L, 12L),
+                 Arrays.asList("c", 2L, 6L, 3L, 11L),
+                 Arrays.asList("c", 2L, 6L, 4L, 12L)
         );
     }
 
@@ -192,14 +113,8 @@ public class JoinFunctionsTest {
 
         engine.eval("result := left_join(ds1, ds2);");
         assertThat(((Dataset) engine.getContext().getAttribute("result")).getDataAsMap()).containsExactlyInAnyOrder(
-                Map.of("pseudo", "Hadrien", "age", 11L),
-                Map.of("pseudo", "Nico", "age", 11L)
-        );
-
-        engine.eval("result := left_join(ds1, ds2 apply ds1 + ds2);");
-        assertThat(((Dataset) engine.getContext().getAttribute("result")).getDataAsMap()).containsExactlyInAnyOrder(
-                Map.of("pseudo", "Hadrien", "age", 30L),
-                Map.of("pseudo", "Nico", "age", 30L)
+                Map.of("name", "Hadrien", "ds1#age",10L, "ds2#age",20L),
+                Map.of("name", "Nico", "ds1#age",11L, "ds2#age",22L)
         );
 
     }
