63662e0994e811b3791f08408dd558043120275c
Hadrien Kohl <hadrien.kohl@gmail.com>
2021-12-03 15:49:29 +0100

    Clean up module setup

--- vtl-jdbc/src/main/java/module-info.java
+++ vtl-jdbc/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.insee.vtl.jdbc {
     requires fr.insee.vtl.model;
     requires java.sql;
+    requires java.scripting;
 
-    requires transitive java.scripting;
+    exports fr.insee.vtl.jdbc;
 }
