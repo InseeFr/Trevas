662a05503f784730667159d16c5be1c6020a98d6
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-03-08 09:40:21 +0100

    change input data type to List<ImmutableEntry>

--- vtl-spark/src/main/java/fr/insee/vtl/spark/SparkAggrFuncExprBuilder.java
+++ vtl-spark/src/main/java/fr/insee/vtl/spark/SparkAggrFuncExprBuilder.java
@@ -7,16 +7,18 @@ import java.util.*;
 import static org.apache.spark.sql.functions.*;
 
 public class SparkAggrFuncExprBuilder {
-    private final Map<String, List<String>> operations;
-    private final Map<String, List<String>> aliases;
+
+    private final List<AbstractMap.SimpleImmutableEntry<String, String>> operations;
+    private final List<AbstractMap.SimpleImmutableEntry<String, String>> aliases;
     private List<Column> tailExpressions;
     private Column headExpression;
-    private final int accuracy=1000000;
+    private final int accuracy = 1000000;
 
-    public SparkAggrFuncExprBuilder(Map<String, List<String>> operations, Map<String,List<String>> aliases) throws Exception {
-          this.operations=operations;
-          this.aliases=aliases;
-          this.parseExpressions();
+    public SparkAggrFuncExprBuilder(List<AbstractMap.SimpleImmutableEntry<String, String>> operations,
+                                    List<AbstractMap.SimpleImmutableEntry<String, String>> aliases) throws Exception {
+        this.operations = operations;
+        this.aliases = aliases;
+        this.parseExpressions();
     }
 
     public List<Column> getTailExpressions() {
@@ -28,74 +30,56 @@ public class SparkAggrFuncExprBuilder {
     }
 
     private void parseExpressions() throws Exception {
-        tailExpressions=new ArrayList<>();
-        String headKey="";
-        Iterator<String> iterator = this.operations.keySet().iterator();
-        // get the head(first) expression
-        if (iterator.hasNext()) {
-            headKey = iterator.next();
-            // get head expression
-            headExpression=buildExpression(headKey,operations.get(headKey).get(0),aliases.get(headKey).get(0));
-        }
+        tailExpressions = new ArrayList<>();
+        // get head expression
+        headExpression = buildExpression(operations.get(0).getKey(), operations.get(0).getValue(), aliases.get(0).getValue());
 
-        iterator=this.operations.keySet().iterator();
         // get the tail expression
-        while (iterator.hasNext()) {
-            String key = iterator.next();
-            // if the key is headKey, skip the first action
-            List<String> functions = operations.get(key);
-            if (key.equals(headKey)){
-                if (functions.size()>1){
-                    for (int i=1;i<functions.size();i++){
-                        tailExpressions.add(buildExpression(headKey,operations.get(headKey).get(i),aliases.get(headKey).get(i)));
-                    }
-                }
-            }
-            else {
-                for (int i=0;i<functions.size();i++){
-                    tailExpressions.add(buildExpression(key,operations.get(key).get(i),aliases.get(key).get(i)));
-                }
-            }
-
+        int index = 1;
+        while (index < operations.size()) {
+            tailExpressions.add(buildExpression(operations.get(index).getKey(), operations.get(index).getValue(), aliases.get(index).getValue()));
+            index++;
         }
+
+
     }
 
-    private Column buildExpression(String colName,String action,String aliasColName) throws Exception {
+    private Column buildExpression(String colName, String action, String aliasColName) throws Exception {
 
         Column expression;
         switch (action.toLowerCase(Locale.ROOT)) {
             case "min":
-                expression= min(colName).alias(aliasColName);
+                expression = min(colName).alias(aliasColName);
                 break;
             case "max":
-                expression= max(colName).alias(aliasColName);
+                expression = max(colName).alias(aliasColName);
                 break;
             case "avg":
-                expression= avg(colName).alias(aliasColName);
+                expression = avg(colName).alias(aliasColName);
                 break;
             case "sum":
-                expression= sum(colName).alias(aliasColName);
+                expression = sum(colName).alias(aliasColName);
                 break;
             case "count":
-                expression= count("*").alias(aliasColName);
+                expression = count("*").alias(aliasColName);
                 break;
             case "median":
-                expression=  percentile_approx(col(colName), lit(0.5), lit(accuracy)).alias(aliasColName);
+                expression = percentile_approx(col(colName), lit(0.5), lit(accuracy)).alias(aliasColName);
                 break;
             case "stddev_pop":
-                expression= stddev_pop(colName).alias(aliasColName);
+                expression = stddev_pop(colName).alias(aliasColName);
                 break;
             case "stddev_samp":
-                expression= stddev_samp(colName).alias(aliasColName);
+                expression = stddev_samp(colName).alias(aliasColName);
                 break;
             case "var_pop":
-                expression= var_pop(colName).alias(aliasColName);
+                expression = var_pop(colName).alias(aliasColName);
                 break;
             case "var_samp":
-                expression= var_samp(colName).alias(aliasColName);
+                expression = var_samp(colName).alias(aliasColName);
                 break;
             case "collect_list":
-                expression= collect_list(colName).alias(aliasColName);
+                expression = collect_list(colName).alias(aliasColName);
                 break;
             default:
                 throw new Exception("Unknown aggregation action");
