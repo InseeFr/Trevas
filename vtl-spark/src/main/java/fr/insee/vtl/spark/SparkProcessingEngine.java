524a3e6d930ea0111009c54c45e9280dd8a56f88
NicoLaval <nicolas.laval@insee.fr>
2022-04-04 21:50:45 +0200

    Fix minor issue

--- vtl-spark/src/main/java/fr/insee/vtl/spark/SparkProcessingEngine.java
+++ vtl-spark/src/main/java/fr/insee/vtl/spark/SparkProcessingEngine.java
@@ -198,11 +198,7 @@ public class SparkProcessingEngine implements ProcessingEngine {
     public DatasetExpression executeAggr(DatasetExpression dataset, Structured.DataStructure structure,
                                          Map<String, AggregationExpression> collectorMap,
                                          Function<Structured.DataPoint, Map<String, Object>> keyExtractor) {
-        return executeAggr(
-                asSparkDataset(dataset),
-                List.of("foo", "bar"),
-                Map.of("col1", "sum", "col2", "avg"),
-                Map.of("col1", "alias1", "col2", "alias2"));
+        throw new UnsupportedOperationException("TODO");
     }
 
     public DatasetExpression executeAggr(SparkDataset dataset, List<String> groupByColumns,
