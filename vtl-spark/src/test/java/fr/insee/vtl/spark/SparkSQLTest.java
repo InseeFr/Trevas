5aab800fa2cb9296864d702686af78e8afe38c95
NicoLaval <nicolas.laval@insee.fr>
2022-04-05 17:55:24 +0200

    Impove Spark JDBC test

--- vtl-spark/src/test/java/fr/insee/vtl/spark/SparkSQLTest.java
+++ vtl-spark/src/test/java/fr/insee/vtl/spark/SparkSQLTest.java
@@ -12,9 +12,13 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,7 +53,7 @@ public class SparkSQLTest {
         statement.executeUpdate("insert into ds1 values (1, 'string1', 'string1', 1, 1, 1.2, 1.2, 'true', 1.2, 100)");
         statement.executeUpdate("insert into ds1 values (2, 'string2', 'string2', 2, 2, 5.2, 5.2, 'false', 5.2, 200)");
         statement.executeUpdate("insert into ds1 values (3, 'string3', 'string3', 3, 3, 3.2, 3.2, 'true', 3.2, 300)");
-        statement.executeUpdate("insert into ds1 values (4, 'string4', 'string4', 4, 4, 4.2, 1.2, 'false', 4.2, 400)");
+        statement.executeUpdate("insert into ds1 values (4, 'string4', 'string4', 4, 4, 4.2, 4.2, 'false', 4.2, 400)");
 
         spark = SparkSession.builder()
                 .appName("test")
@@ -84,27 +88,26 @@ public class SparkSQLTest {
                 "col7 := not(COLBOOLEAN), " +
                 "col8 := COLNUMERIC + 0.0001, " +
                 "col9 := COLBIGINT + 9999 " +
-                "];");
+                "]" +
+                "[keep ID, col1, col2, col3, col4, col5, col6, col7, col8, col9];");
 
         var ds2 = (Dataset) bindings.get("ds2");
+        List<List<Object>> roundedDs2 = ds2.getDataAsList().stream().map(line ->
+                line.stream().map(element -> {
+                    if (element instanceof Double) {
+                        BigDecimal bd = new BigDecimal(Double.toString((Double) element));
+                        bd = bd.setScale(2, RoundingMode.HALF_UP);
+                        return bd.doubleValue();
+                    }
+                    return element;
+                }).collect(Collectors.toList())
+        ).collect(Collectors.toList());
 
-        assertThat(ds2.getDataAsMap()).containsExactly(
-                Map.of("ID", 1L, "COL1", "string1 ok", "COL2", "string1 ok",
-                        "COL3", 2L, "COL4", 100L, "COL5", 1.1D,
-                        "COL6", 1.1991D, "COL7", false,
-                        "COL8", 1.2001D, "COL9", 10099L),
-                Map.of("ID", 2L, "COL1", "string2 ok", "COL2", "string2 ok",
-                        "COL3", 3L, "COL4", 200L, "COL5", 5.1D,
-                        "COL6", 5.1991D, "COL7", true,
-                        "COL8", 5.2001D, "COL9", 10199L),
-                Map.of("ID", 3L, "COL1", "string3 ok", "COL2", "string3 ok",
-                        "COL3", 4L, "COL4", 300L, "COL5", 3.1D,
-                        "COL6", 3.1991D, "COL7", false,
-                        "COL8", 3.2001D, "COL9", 10299L),
-                Map.of("ID", 5L, "COL1", "string4 ok", "COL2", "string4 ok",
-                        "COL3", 4L, "COL4", 400L, "COL5", 4.1D,
-                        "COL6", 4.1991D, "COL7", true,
-                        "COL8", 4.2001D, "COL9", 10399L)
+        assertThat(roundedDs2).containsExactlyInAnyOrder(
+                List.of(1L, "string1 ok", "string1 ok", 2L, 100L, 1.1D, 1.2D, false, 1.2001D, 10099L),
+                List.of(2L, "string2 ok", "string2 ok", 3L, 200L, 5.1D, 5.2D, true, 5.2001D, 10199L),
+                List.of(3L, "string3 ok", "string3 ok", 4L, 300L, 3.1D, 3.2D, false, 3.2001D, 10299L),
+                List.of(4L, "string4 ok", "string4 ok", 5L, 400L, 4.1D, 4.2D, true, 4.2001D, 10399L)
         );
     }
 }
