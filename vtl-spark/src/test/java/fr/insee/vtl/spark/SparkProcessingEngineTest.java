22a2063d92ecd299a9cd5489409448e2721e0aa0
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-07-01 19:13:12 +0200

    add median unit test

--- vtl-spark/src/test/java/fr/insee/vtl/spark/SparkProcessingEngineTest.java
+++ vtl-spark/src/test/java/fr/insee/vtl/spark/SparkProcessingEngineTest.java
@@ -1724,8 +1724,232 @@ public class SparkProcessingEngineTest {
      *
      * */
 
+    @Test
+    public void testAnMedianWithPartitionClause() throws ScriptException {
+
+        // Analytical function Test case 1 : median on window with partition
+        /* Input dataset
+        *   +----+----+----+----+----+
+            |Id_1|Id_2|Year|Me_1|Me_2|
+            +----+----+----+----+----+
+            |   A|  XX|2000|   3| 1.0|
+            |   A|  XX|2001|   4| 9.0|
+            |   A|  XX|2002|   7| 5.0|
+            |   A|  XX|2003|   6| 8.0|
+            |   A|  YY|2000|   9| 3.0|
+            |   A|  YY|2001|   5| 4.0|
+            |   A|  YY|2002|  10| 2.0|
+            |   A|  YY|2003|   5| 7.0|
+            +----+----+----+----+----+
+        * */
+        ScriptContext context = engine.getContext();
+        context.setAttribute("ds1", anCountDS1 , ScriptContext.ENGINE_SCOPE);
+
+
+        engine.eval("res := median ( ds1 over ( partition by Id_1, Id_2 ) )");
+        assertThat(engine.getContext().getAttribute("res")).isInstanceOf(Dataset.class);
+
+        /*
+        *
+        *   +----+----+----+----+----+----------+----------+
+            |Id_1|Id_2|Year|Me_1|Me_2|median_Me_1|median_Me_2|
+            +----+----+----+----+----+-----------+-----------+
+            |   A|  XX|2000|   3| 1.0|          4|        5.0|
+            |   A|  XX|2001|   4| 9.0|          4|        5.0|
+            |   A|  XX|2002|   7| 5.0|          4|        5.0|
+            |   A|  XX|2003|   6| 8.0|          4|        5.0|
+            |   A|  YY|2000|   9| 3.0|          5|        3.0|
+            |   A|  YY|2001|   5| 4.0|          5|        3.0|
+            |   A|  YY|2002|  10| 2.0|          5|        3.0|
+            |   A|  YY|2003|   5| 7.0|          5|        3.0|
+            +----+----+----+----+----+-----------+-----------+
+        * */
+        assertThat(((Dataset) engine.getContext().getAttribute("res")).getDataAsMap()).containsExactly(
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2000L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2001L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2002L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2003L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2000L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2001L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2002L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2003L, "Me_1", 5L,"Me_2",3.0D)
+        );
+
+    }
+
+    @Test
+    public void testAnMedianWithPartitionOrderByClause() throws ScriptException {
+
+        // Analytical function Test case 2 : median on window with partition and order by
+        /* Input dataset
+        *   +----+----+----+----+----+
+            |Id_1|Id_2|Year|Me_1|Me_2|
+            +----+----+----+----+----+
+            |   A|  XX|2000|   3| 1.0|
+            |   A|  XX|2001|   4| 9.0|
+            |   A|  XX|2002|   7| 5.0|
+            |   A|  XX|2003|   6| 8.0|
+            |   A|  YY|2000|   9| 3.0|
+            |   A|  YY|2001|   5| 4.0|
+            |   A|  YY|2002|  10| 2.0|
+            |   A|  YY|2003|   5| 7.0|
+            +----+----+----+----+----+
+        * */
+        ScriptContext context = engine.getContext();
+        context.setAttribute("ds1", anCountDS1 , ScriptContext.ENGINE_SCOPE);
+
+
+        engine.eval("res := median ( ds1 over ( partition by Id_1, Id_2 order by Year) )");
+        assertThat(engine.getContext().getAttribute("res")).isInstanceOf(Dataset.class);
+
+        /*
+        *   +----+----+----+----+----+----------+----------+
+            |Id_1|Id_2|Year|Me_1|Me_2|median_Me_1|median_Me_2|
+            +----+----+----+----+----+-----------+-----------+
+            |   A|  XX|2000|   3| 1.0|          3|        1.0|
+            |   A|  XX|2001|   4| 9.0|          3|        1.0|
+            |   A|  XX|2002|   7| 5.0|          4|        5.0|
+            |   A|  XX|2003|   6| 8.0|          4|        5.0|
+            |   A|  YY|2000|   9| 3.0|          9|        3.0|
+            |   A|  YY|2001|   5| 4.0|          5|        3.0|
+            |   A|  YY|2002|  10| 2.0|          9|        3.0|
+            |   A|  YY|2003|   5| 7.0|          5|        3.0|
+            +----+----+----+----+----+-----------+-----------+
+
+        * */
+        assertThat(((Dataset) engine.getContext().getAttribute("res")).getDataAsMap()).containsExactly(
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2000L, "Me_1", 3L,"Me_2",1.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2001L, "Me_1", 3L,"Me_2",1.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2002L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2003L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2000L, "Me_1", 9L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2001L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2002L, "Me_1", 9L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2003L, "Me_1", 5L,"Me_2",3.0D)
+        );
+
+    }
+
+
+    @Test
+    public void testAnMedianWithPartitionOrderByDPClause() throws ScriptException {
+
+        // Analytical function count test case 3 : median on window with partition, orderBy and data points
+        /* Input dataset
+        *   +----+----+----+----+----+
+            |Id_1|Id_2|Year|Me_1|Me_2|
+            +----+----+----+----+----+
+            |   A|  XX|2000|   3| 1.0|
+            |   A|  XX|2001|   4| 9.0|
+            |   A|  XX|2002|   7| 5.0|
+            |   A|  XX|2003|   6| 8.0|
+            |   A|  YY|2000|   9| 3.0|
+            |   A|  YY|2001|   5| 4.0|
+            |   A|  YY|2002|  10| 2.0|
+            |   A|  YY|2003|   5| 7.0|
+            +----+----+----+----+----+
+        * */
+        ScriptContext context = engine.getContext();
+        context.setAttribute("ds1", anCountDS1 , ScriptContext.ENGINE_SCOPE);
+
+        engine.eval("res := median ( ds1 over ( partition by Id_1 order by Id_2 data points between 2 preceding and 2 following) )");
+        assertThat(engine.getContext().getAttribute("res")).isInstanceOf(Dataset.class);
+
+        /*
+        * The result data frame
+        *
+        *   +----+----+----+----+----+----------+----------+
+            |Id_1|Id_2|Year|Me_1|Me_2|median_Me_1|median_Me_2|
+            +----+----+----+----+----+-----------+-----------+
+            |   A|  XX|2000|   3| 1.0|          4|        5.0|
+            |   A|  XX|2001|   4| 9.0|          4|        5.0|
+            |   A|  XX|2002|   7| 5.0|          6|        5.0|
+            |   A|  XX|2003|   6| 8.0|          6|        5.0|
+            |   A|  YY|2000|   9| 3.0|          7|        4.0|
+            |   A|  YY|2001|   5| 4.0|          6|        4.0|
+            |   A|  YY|2002|  10| 2.0|          5|        3.0|
+            |   A|  YY|2003|   5| 7.0|          5|        4.0|
+            +----+----+----+----+----+-----------+-----------+
+
+        * */
+        assertThat(((Dataset) engine.getContext().getAttribute("res")).getDataAsMap()).containsExactly(
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2000L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2001L, "Me_1", 4L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2002L, "Me_1", 6L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2003L, "Me_1", 6L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2000L, "Me_1", 7L,"Me_2",4.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2001L, "Me_1", 6L,"Me_2",4.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2002L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2003L, "Me_1", 5L,"Me_2",4.0D)
+        );
+
+    }
+
+    @Test
+    public void testAnMedianWithPartitionOrderByRangeClause() throws ScriptException {
+
+        // Analytical function count test case 4 : median on window with partition, orderBy and range
+        /* Input dataset
+        *   +----+----+----+----+----+
+            |Id_1|Id_2|Year|Me_1|Me_2|
+            +----+----+----+----+----+
+            |   A|  XX|2000|   3| 1.0|
+            |   A|  XX|2001|   4| 9.0|
+            |   A|  XX|2002|   7| 5.0|
+            |   A|  XX|2003|   6| 8.0|
+            |   A|  YY|2000|   9| 3.0|
+            |   A|  YY|2001|   5| 4.0|
+            |   A|  YY|2002|  10| 2.0|
+            |   A|  YY|2003|   5| 7.0|
+            +----+----+----+----+----+
+        * */
+        ScriptContext context = engine.getContext();
+        context.setAttribute("ds1", anCountDS1 , ScriptContext.ENGINE_SCOPE);
+
+
+        engine.eval("res := avg ( ds1 over ( partition by Id_1 order by Year range between -1 and 1) )");
+        assertThat(engine.getContext().getAttribute("res")).isInstanceOf(Dataset.class);
+
+        /*
+        * The result data frame
+        *
+        *   +----+----+----+----+----+----------+----------+
+            |Id_1|Id_2|Year|Me_1|Me_2|median_Me_1|median_Me_2|
+            +----+----+----+----+----+-----------+-----------+
+            |   A|  XX|2000|   3| 1.0|          4|        3.0|
+            |   A|  YY|2000|   9| 3.0|          4|        3.0|
+            |   A|  XX|2001|   4| 9.0|          5|        3.0|
+            |   A|  YY|2001|   5| 4.0|          5|        3.0|
+            |   A|  XX|2002|   7| 5.0|          5|        5.0|
+            |   A|  YY|2002|  10| 2.0|          5|        5.0|
+            |   A|  XX|2003|   6| 8.0|          6|        5.0|
+            |   A|  YY|2003|   5| 7.0|          6|        5.0|
+            +----+----+----+----+----+-----------+-----------+
+
+        * */
+        assertThat(((Dataset) engine.getContext().getAttribute("res")).getDataAsMap()).containsExactly(
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2000L, "Me_1", 4L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2001L, "Me_1", 4L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2002L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "XX", "Year", 2003L, "Me_1", 5L,"Me_2",3.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2000L, "Me_1", 5L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2001L, "Me_1", 5L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2002L, "Me_1", 6L,"Me_2",5.0D),
+                Map.of("Id_1", "A", "Id_2", "YY", "Year", 2003L, "Me_1", 6L,"Me_2",5.0D)
+        );
+
+    }
+
     /*
      * End of Median test case */
+
+    /*
+     * Test case for analytic function stddev_pop
+     *
+     * */
+
+    /*
+     * End of stddev_pop test case */
 
     @Test
     public void testRename() throws ScriptException {
