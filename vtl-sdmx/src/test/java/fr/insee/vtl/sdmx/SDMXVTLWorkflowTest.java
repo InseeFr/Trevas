c4359888bc05de3982a8cbda14c256152e89e39d
Nicolas Laval <nicolas.laval@making-sense.info>
2024-05-27 09:14:26 +0200

    Update SDMXVTLWorkflowTest.java

--- vtl-sdmx/src/test/java/fr/insee/vtl/sdmx/SDMXVTLWorkflowTest.java
+++ vtl-sdmx/src/test/java/fr/insee/vtl/sdmx/SDMXVTLWorkflowTest.java
@@ -81,8 +81,8 @@ public class SDMXVTLWorkflowTest {
         ReadableDataLocation rdl = new ReadableDataLocationTmp("src/test/resources/DSD_BPE_CENSUS.xml");
         SDMXVTLWorkflow sdmxVtlWorkflow = new SDMXVTLWorkflow(engine, rdl, Map.of());
         assertThat(sdmxVtlWorkflow.getRulesetsVTL()).isEqualTo(
-                "define datapoint ruleset UNIQUE_MUNICIPALITY (variable DEPCOM) is\n" +
-                        "                    MUNICIPALITY_FORMAT_RULE : match_characters(DEPCOM, \"[0-9]{5}|2[A-B][0-9]{3}\") errorcode \"Municipality code is not in the correct format\"\n" +
+                "define datapoint ruleset UNIQUE_MUNICIPALITY (valuedomain CL_DEPCOM) is\n" +
+                        "                    MUNICIPALITY_FORMAT_RULE : match_characters(CL_DEPCOM, \"[0-9]{5}|2[A-B][0-9]{3}\") errorcode \"Municipality code is not in the correct format\"\n" +
                         "                end datapoint ruleset;\n" +
                         "\n" +
                         "define datapoint ruleset NUTS3_TYPES (variable facility_type, nb) is\n" +
@@ -97,10 +97,10 @@ public class SDMXVTLWorkflowTest {
         SDMXVTLWorkflow sdmxVtlWorkflow = new SDMXVTLWorkflow(engine, rdl, Map.of());
         assertThat(sdmxVtlWorkflow.getTransformationsVTL()).isEqualTo(
                 "// Validation of municipality code in input file\n" +
-                        "CHECK_MUNICIPALITY := check_datapoint(BPE_DETAIL, UNIQUE_MUNICIPALITY invalid);\n" +
+                        "CHECK_MUNICIPALITY := check_datapoint(BPE_DETAIL_VTL, UNIQUE_MUNICIPALITY invalid);\n" +
                         "\n" +
                         "// Clean BPE input database\n" +
-                        "BPE_DETAIL_CLEAN := BPE_DETAIL  [drop LAMBERT_X, LAMBERT_Y]\n" +
+                        "BPE_DETAIL_CLEAN := BPE_DETAIL_VTL  [drop LAMBERT_X, LAMBERT_Y]\n" +
                         "                            [rename ID_EQUIPEMENT to id, TYPEQU to facility_type, DEPCOM to municipality, REF_YEAR to year];\n" +
                         "\n" +
                         "// BPE aggregation by municipality, type and year\n" +
