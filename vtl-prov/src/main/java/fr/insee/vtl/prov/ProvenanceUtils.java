34fa7ce42bb31d494d4f27a2f82990829d4a5bd8
Hadrien Kohl <hadrien.kohl@gmail.com>
2024-08-29 14:17:07 +0200

    Remove self loops

--- vtl-prov/src/main/java/fr/insee/vtl/prov/ProvenanceUtils.java
+++ vtl-prov/src/main/java/fr/insee/vtl/prov/ProvenanceUtils.java
@@ -12,37 +12,9 @@ import java.util.*;
 
 public class ProvenanceUtils {
 
-    public static ProvenanceListener getProvenance(String script) {
-        CodePointCharStream stream = CharStreams.fromString(script);
-        VtlLexer lexer = new VtlLexer(stream);
-        VtlParser parser = new VtlParser(new CommonTokenStream(lexer));
-
-        ProvenanceListener provenanceListener = new ProvenanceListener();
-        ParseTreeWalker.DEFAULT.walk(provenanceListener, parser.start());
-
-        return provenanceListener;
-    }
-
-    public static void printTree(ProvenanceListener.Node node, String prefix, boolean isLast) {
-        if (node == null) {
-            return;
-        }
-
-        // Print the current node with appropriate formatting
-        System.out.println(prefix + (isLast ? "└── " : "├── ") + node.name + "\t[ " + node.expression + " ]");
-
-        // Get the list of parent nodes
-        Collection<ProvenanceListener.Node> parents = node.parents.values();
-        int count = parents.size();
-        int index = 0;
-
-        // Recursively print each parent node
-        for (ProvenanceListener.Node parent : parents) {
-            printTree(parent, prefix + (isLast ? "    " : "│   "), ++index == count);
-        }
-    }
-
     public static List<Object> toBusinessModel(ProvenanceListener listener) {
+        // TODO: @nico te graph needs to be refactored. I'll try to fix it before monday.
+        
         ArrayList<Object> model = new ArrayList<>();
         LinkedHashMap<String, ProvenanceListener.Node> variables = listener.variables;
         variables.values().forEach(node -> {
