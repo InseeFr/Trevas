4d5b072772ae60605c6a96aea8c70ce175ceb1d0
Hadrien Kohl <hadrien.kohl@gmail.com>
2025-04-18 17:30:11 +0200

    Implement a first eval test

--- coverage/src/test/java/fr/insee/vtl/coverage/TCKTest.java
+++ coverage/src/test/java/fr/insee/vtl/coverage/TCKTest.java
@@ -1,17 +1,27 @@
 package fr.insee.vtl.coverage;
 
 import fr.insee.vtl.coverage.model.Folder;
-import org.junit.jupiter.api.DynamicContainer;
-import org.junit.jupiter.api.DynamicNode;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import fr.insee.vtl.coverage.model.Test;
+import fr.insee.vtl.model.Dataset;
+import org.junit.jupiter.api.*;
 
+import javax.script.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class TCKTest {
+
+    private ScriptEngine engine;
+
+    @BeforeEach
+    public void setUp() {
+        engine = new ScriptEngineManager().getEngineByName("vtl");
+    }
 
     @TestFactory
     Stream<DynamicNode> generateTests() {
@@ -34,8 +44,22 @@ class TCKTest {
             children.add(DynamicTest.dynamicTest(
                     folder.getName(),
                     () -> {
-                        // TODO: Execute the test.
-                        System.out.println("Executing test" + folder.getTest());
+                        Test test = folder.getTest();
+                        Map<String, Dataset> inputs = test.getInput();
+                        Bindings bindings = new SimpleBindings();
+                        bindings.putAll(inputs);
+
+                        engine.getContext().setBindings(bindings, ScriptContext.ENGINE_SCOPE);
+                        engine.eval(test.getScript());
+
+                        Map<String, Dataset> outputs = test.getOutputs();
+                        outputs.forEach((name, value) -> {
+                            Object actualValue = engine.getContext().getAttribute(name);
+                            assertThat(actualValue).isInstanceOf(Dataset.class);
+                            Dataset dataset = (Dataset) actualValue;
+                            assertThat(dataset.getDataStructure()).isEqualTo(value.getDataStructure());
+                            assertThat(dataset.getDataAsList()).containsAnyElementsOf(value.getDataAsList());
+                        });
                     }
             ));
         }
