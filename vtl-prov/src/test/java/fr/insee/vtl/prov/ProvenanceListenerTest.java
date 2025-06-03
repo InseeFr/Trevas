4841dd2f627ac627c0eac8a4b119c9e69a5b6bf7
Nicolas Laval <nicolas.laval@making-sense.info>
2025-06-03 10:09:22 +0200

    Add testWithEmptyLines for provenance module

--- vtl-prov/src/test/java/fr/insee/vtl/prov/ProvenanceListenerTest.java
+++ vtl-prov/src/test/java/fr/insee/vtl/prov/ProvenanceListenerTest.java
@@ -20,4 +20,27 @@ public class ProvenanceListenerTest {
         ProvenanceListener.run(script, "trevas-simple-test", "Simple test from Trevas tests");
     assertThat(program.getProgramSteps()).hasSize(3);
   }
+
+  @Test
+  public void testWithEmptyLines() {
+    String script =
+        """
+
+
+            ds1 := 'data.ds1'[calc identifier id1 := id1, var1 := cast(var1, integer), var2 := cast(var2, integer)];
+
+
+            ds2_out := 'other.ds2'[calc identifier id1 := id1, var1 := cast(var1, integer), var2 := cast(var2, integer)];
+            ds_sum := ds1 + ds2_out;
+            ds_mul <- ds_sum * 3;
+            'data.ds_res' <- ds_mul[filter mod(var1, 2) = 0][calc var_sum := var1 + var2];
+
+
+
+        """;
+
+    Program program =
+        ProvenanceListener.run(script, "trevas-simple-test", "Simple test from Trevas tests");
+    assertThat(program.getProgramSteps()).hasSize(5);
+  }
 }
