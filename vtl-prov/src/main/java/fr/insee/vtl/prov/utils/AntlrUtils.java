756341034f0d8049522985d45ce7b7cf03d30092
Nicolas Laval <nicolas.laval@making-sense.info>
2025-07-19 09:47:08 +0200

    Improve vtl-prov with defines

--- vtl-prov/src/main/java/fr/insee/vtl/prov/utils/AntlrUtils.java
+++ vtl-prov/src/main/java/fr/insee/vtl/prov/utils/AntlrUtils.java
@@ -2,55 +2,68 @@ package fr.insee.vtl.prov.utils;
 
 import fr.insee.vtl.parser.VtlLexer;
 import fr.insee.vtl.parser.VtlParser;
-import java.util.ArrayList;
-import java.util.List;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.Interval;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AntlrUtils {
 
-  public static List<String> getDefineStatements(String script) {
-    VtlParser parser = getParser(script);
-    VtlParser.StartContext context = parser.start();
-    CharStream input = parser.getTokenStream().getTokenSource().getInputStream();
+    public static Map<String, String> getDefineStatements(String script) {
+        VtlParser parser = getParser(script);
+        VtlParser.StartContext context = parser.start();
+        CharStream input = parser.getTokenStream().getTokenSource().getInputStream();
 
-    List<String> defineExpressions = new ArrayList<>();
+        Map<String, String> defineExpressions = new HashMap<>();
 
-    for (VtlParser.StatementContext stmt : context.statement()) {
-      if (stmt instanceof VtlParser.DefineExpressionContext) {
-        // We add ";" to recompose statement
-        defineExpressions.add(getOriginalText(stmt, input) + ";");
-      }
+        for (VtlParser.StatementContext stmt : context.statement()) {
+            if (stmt instanceof VtlParser.DefineExpressionContext defineCtx) {
+                VtlParser.DefOperatorsContext defOp = defineCtx.defOperators();
+
+                if (defOp instanceof VtlParser.DefDatapointRulesetContext dprCtx) {
+                    String rulesetName = dprCtx.rulesetID().getText();
+                    String originalScript = getOriginalText(dprCtx, input) + ";";
+                    defineExpressions.put(rulesetName, originalScript);
+                } else if (defOp instanceof VtlParser.DefHierarchicalContext hierCtx) {
+                    String rulesetName = hierCtx.rulesetID().getText();
+                    String originalScript = getOriginalText(hierCtx, input) + ";";
+                    defineExpressions.put(rulesetName, originalScript);
+                }
+            }
+        }
+
+        return defineExpressions;
     }
-    return defineExpressions;
-  }
 
-  public static List<String> getAssignmentStatements(String script) {
-    VtlParser parser = getParser(script);
-    VtlParser.StartContext context = parser.start();
-    CharStream input = parser.getTokenStream().getTokenSource().getInputStream();
+    public static List<String> getAssignmentStatements(String script) {
+        VtlParser parser = getParser(script);
+        VtlParser.StartContext context = parser.start();
+        CharStream input = parser.getTokenStream().getTokenSource().getInputStream();
 
-    List<String> assignmentExpressions = new ArrayList<>();
+        List<String> assignmentExpressions = new ArrayList<>();
 
-    for (VtlParser.StatementContext stmt : context.statement()) {
-      if (stmt instanceof VtlParser.TemporaryAssignmentContext
-          || stmt instanceof VtlParser.PersistAssignmentContext) {
-        // We add ";" to recompose statement
-        assignmentExpressions.add(getOriginalText(stmt, input) + ";");
-      }
+        for (VtlParser.StatementContext stmt : context.statement()) {
+            if (stmt instanceof VtlParser.TemporaryAssignmentContext
+                    || stmt instanceof VtlParser.PersistAssignmentContext) {
+                // We add ";" to recompose statement
+                assignmentExpressions.add(getOriginalText(stmt, input) + ";");
+            }
+        }
+        return assignmentExpressions;
     }
-    return assignmentExpressions;
-  }
 
-  private static VtlParser getParser(String script) {
-    CodePointCharStream stream = CharStreams.fromString(script);
-    VtlLexer lexer = new VtlLexer(stream);
-    return new VtlParser(new CommonTokenStream(lexer));
-  }
+    private static VtlParser getParser(String script) {
+        CodePointCharStream stream = CharStreams.fromString(script);
+        VtlLexer lexer = new VtlLexer(stream);
+        return new VtlParser(new CommonTokenStream(lexer));
+    }
 
-  private static String getOriginalText(ParserRuleContext ctx, CharStream input) {
-    int startIdx = ctx.getStart().getStartIndex();
-    int stopIdx = ctx.getStop().getStopIndex();
-    return input.getText(Interval.of(startIdx, stopIdx));
-  }
+    private static String getOriginalText(ParserRuleContext ctx, CharStream input) {
+        int startIdx = ctx.getStart().getStartIndex();
+        int stopIdx = ctx.getStop().getStopIndex();
+        return input.getText(Interval.of(startIdx, stopIdx));
+    }
 }
--- vtl-prov/src/test/java/fr/insee/vtl/prov/utils/AntlrUtilsTest.java
+++ vtl-prov/src/test/java/fr/insee/vtl/prov/utils/AntlrUtilsTest.java
@@ -1,43 +1,44 @@
 package fr.insee.vtl.prov.utils;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AntlrUtilsTest {
 
-  String simpleScript =
-      """
-                  ds_sum := ds1 + ds2;
-                  ds_mul := ds_sum * 3;\s
-                  ds_res <- ds_mul[filter mod(var1, 2) = 0][calc var_sum := var1 + var2];\
-            """;
+    String simpleScript =
+            """
+                          ds_sum := ds1 + ds2;
+                          ds_mul := ds_sum * 3;\s
+                          ds_res <- ds_mul[filter mod(var1, 2) = 0][calc var_sum := var1 + var2];\
+                    """;
 
-  String simpleValidation =
-      """
-                  define datapoint ruleset test (variable sex) is
-                      myrule : sex in {"M"} errorcode "ERROR"
-                  end datapoint ruleset;
-                  pengfei.ds_result <- check_datapoint(pengfei.pengfei, test);
-            """;
+    String simpleValidation =
+            """
+                          define datapoint ruleset test (variable sex) is
+                              myrule : sex in {"M"} errorcode "ERROR"
+                          end datapoint ruleset;
+                          pengfei.ds_result <- check_datapoint(pengfei.pengfei, test);
+                    """;
 
-  @Test
-  public void getDefineStatementsTest() {
-    assertThat(AntlrUtils.getDefineStatements(simpleScript).size()).isEqualTo(0);
+    @Test
+    public void getDefineStatementsTest() {
+        assertThat(AntlrUtils.getDefineStatements(simpleScript).size()).isEqualTo(0);
 
-    assertThat(AntlrUtils.getDefineStatements(simpleValidation).size()).isEqualTo(1);
-    assertThat(AntlrUtils.getDefineStatements(simpleValidation).get(0))
-        .contains("define datapoint ruleset test (variable sex) is");
-  }
+        assertThat(AntlrUtils.getDefineStatements(simpleValidation).size()).isEqualTo(1);
+        assertThat(AntlrUtils.getDefineStatements(simpleValidation).get("test"))
+                .contains("define datapoint ruleset test (variable sex) is");
+    }
 
-  @Test
-  public void getAssignmentStatementsTest() {
-    List<String> simpleScriptAssignmentStatements =
-        AntlrUtils.getAssignmentStatements(simpleScript);
-    assertThat(simpleScriptAssignmentStatements.size()).isEqualTo(3);
-    assertThat(simpleScriptAssignmentStatements.get(0)).isEqualTo("ds_sum := ds1 + ds2;");
+    @Test
+    public void getAssignmentStatementsTest() {
+        List<String> simpleScriptAssignmentStatements =
+                AntlrUtils.getAssignmentStatements(simpleScript);
+        assertThat(simpleScriptAssignmentStatements.size()).isEqualTo(3);
+        assertThat(simpleScriptAssignmentStatements.get(0)).isEqualTo("ds_sum := ds1 + ds2;");
 
-    assertThat(AntlrUtils.getAssignmentStatements(simpleValidation).size()).isEqualTo(1);
-  }
+        assertThat(AntlrUtils.getAssignmentStatements(simpleValidation).size()).isEqualTo(1);
+    }
 }
