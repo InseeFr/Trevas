26499cb4c17e196e98e2bdcec82c7a025dd1faf3
Nicolas Laval <nicolas.laval@making-sense.info>
2025-07-19 08:01:03 +0200

    Format vtl-prov AntlrUtils

--- vtl-prov/src/test/java/fr/insee/vtl/prov/utils/AntlrUtilsTest.java
+++ vtl-prov/src/test/java/fr/insee/vtl/prov/utils/AntlrUtilsTest.java
@@ -1,39 +1,43 @@
 package fr.insee.vtl.prov.utils;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
 
 public class AntlrUtilsTest {
 
-    String simpleScript = """
+  String simpleScript =
+      """
                   ds_sum := ds1 + ds2;
                   ds_mul := ds_sum * 3;\s
                   ds_res <- ds_mul[filter mod(var1, 2) = 0][calc var_sum := var1 + var2];\
             """;
 
-    String simpleValidation = """
+  String simpleValidation =
+      """
                   define datapoint ruleset test (variable sex) is
                       myrule : sex in {"M"} errorcode "ERROR"
                   end datapoint ruleset;
                   pengfei.ds_result <- check_datapoint(pengfei.pengfei, test);
             """;
 
-    @Test
-    public void getDefineStatementsTest() {
-        assertThat(AntlrUtils.getDefineStatements(simpleScript).size()).isEqualTo(0);
+  @Test
+  public void getDefineStatementsTest() {
+    assertThat(AntlrUtils.getDefineStatements(simpleScript).size()).isEqualTo(0);
 
-        assertThat(AntlrUtils.getDefineStatements(simpleValidation).size()).isEqualTo(1);
-    }
+    assertThat(AntlrUtils.getDefineStatements(simpleValidation).size()).isEqualTo(1);
+    assertThat(AntlrUtils.getDefineStatements(simpleValidation).get(0))
+        .contains("define datapoint ruleset test (variable sex) is");
+  }
 
-    @Test
-    public void getAssignmentStatementsTest() {
-        List<String> simpleScriptAssignmentStatements = AntlrUtils.getAssignmentStatements(simpleScript);
-        assertThat(simpleScriptAssignmentStatements.size()).isEqualTo(3);
-        assertThat(simpleScriptAssignmentStatements.get(0)).isEqualTo("ds_sum := ds1 + ds2;");
+  @Test
+  public void getAssignmentStatementsTest() {
+    List<String> simpleScriptAssignmentStatements =
+        AntlrUtils.getAssignmentStatements(simpleScript);
+    assertThat(simpleScriptAssignmentStatements.size()).isEqualTo(3);
+    assertThat(simpleScriptAssignmentStatements.get(0)).isEqualTo("ds_sum := ds1 + ds2;");
 
-        assertThat(AntlrUtils.getAssignmentStatements(simpleValidation).size()).isEqualTo(1);
-    }
+    assertThat(AntlrUtils.getAssignmentStatements(simpleValidation).size()).isEqualTo(1);
+  }
 }
