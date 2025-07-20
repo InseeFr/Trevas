c24b446d8a4a49a8cc1319eb3b7a1990055280fc
Nicolas Laval <nicolas.laval@making-sense.info>
2025-07-20 07:56:11 +0200

    Fix/provenance validation (#424)
    * Add testWithEmptyLines for provenance module
    
    * Minor improvements in ProvenanceListener
    
    * Refactor run and delete runWithBindings in vtl-prov module
    
    * Init AntlrUtils in vtl-prov
    
    * Format vtl-prov AntlrUtils
    
    * Improve ProvenanceListenerTest
    
    * Fix testWithEmptyLines in vtl-prov
    
    * Improve vtl-prov with defines
    
    * Complete RDFTest validation script
    
    * Format

--- vtl-prov/src/test/java/fr/insee/vtl/prov/RDFTest.java
+++ vtl-prov/src/test/java/fr/insee/vtl/prov/RDFTest.java
@@ -5,14 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import fr.insee.vtl.engine.VtlScriptEngine;
 import fr.insee.vtl.model.Dataset;
 import fr.insee.vtl.model.InMemoryDataset;
+import fr.insee.vtl.model.Structured;
 import fr.insee.vtl.prov.prov.Program;
 import fr.insee.vtl.prov.utils.PropertiesLoader;
 import fr.insee.vtl.prov.utils.RDFUtils;
+import fr.insee.vtl.sdmx.TrevasSDMXUtils;
+import fr.insee.vtl.spark.SparkDataset;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import org.apache.jena.rdf.model.Model;
@@ -21,6 +25,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RDFTest {
+
+  private SparkSession spark;
+  private ScriptEngine engine;
 
   static Properties conf;
 
@@ -52,6 +59,13 @@ public class RDFTest {
     Model model = modelClass.add(modelDetail).add(modelLink);
     RDFUtils.loadModelWithCredentials(
         model, sparqlEndpoint, sparqlEndpointUser, sparlqEndpointPassword);
+
+    spark = SparkSession.builder().appName("test").master("local").getOrCreate();
+
+    ScriptEngineManager mgr = new ScriptEngineManager();
+    engine = mgr.getEngineByExtension("vtl");
+    engine.put(VtlScriptEngine.PROCESSING_ENGINE_NAMES, "spark");
+    engine.put("$vtl.spark.session", spark);
   }
 
   @BeforeEach
@@ -68,14 +82,45 @@ public class RDFTest {
 
     String script =
         """
-                ds_sum := ds1 + ds2;
-                ds_mul := ds_sum * 3;\s
-                ds_res <- ds_mul   [filter mod(var1, 2) = 0]\
-                                   [calc var_sum := var1 + var2];\
-                """;
+                        ds_sum := ds1 + ds2;
+                        ds_mul := ds_sum * 3;\s
+                        ds_res <- ds_mul   [filter mod(var1, 2) = 0]\
+                                           [calc var_sum := var1 + var2];\
+                        """;
+    Map<String, Class<?>> types =
+        Map.of("id", String.class, "var1", Long.class, "var2", Long.class);
+    Map<String, Dataset.Role> roles =
+        Map.of(
+            "id",
+            Dataset.Role.IDENTIFIER,
+            "var1",
+            Dataset.Role.MEASURE,
+            "var2",
+            Dataset.Role.MEASURE);
+    InMemoryDataset ds1 =
+        new InMemoryDataset(
+            List.of(
+                Map.of("id", "1", "var1", 10L, "var2", 11L),
+                Map.of("id", "2", "var1", 11L, "var2", 10L),
+                Map.of("id", "3", "var1", 12L, "var2", 9L)),
+            types,
+            roles);
+    InMemoryDataset ds2 =
+        new InMemoryDataset(
+            List.of(
+                Map.of("id", "1", "var1", 20L, "var2", 110L),
+                Map.of("id", "2", "var1", -1L, "var2", 10L),
+                Map.of("id", "3", "var1", 0L, "var2", 9L)),
+            types,
+            roles);
+
+    ScriptContext context = engine.getContext();
+    context.setAttribute("ds1", ds1, ScriptContext.ENGINE_SCOPE);
+    context.setAttribute("ds2", ds2, ScriptContext.ENGINE_SCOPE);
 
     Program program =
-        ProvenanceListener.run(script, "trevas-simple-test", "Simple test from Trevas tests");
+        ProvenanceListener.run(
+            engine, script, "trevas-simple-test", "Simple test from Trevas tests");
     Model model = RDFUtils.buildModel(program);
     String content = RDFUtils.serialize(model, "JSON-LD");
     assertThat(content).isNotEmpty();
@@ -86,105 +131,85 @@ public class RDFTest {
   }
 
   @Test
-  public void simpleTestWithBindings() throws IOException {
-    SparkSession spark = SparkSession.builder().appName("test").master("local").getOrCreate();
-
-    ScriptEngineManager mgr = new ScriptEngineManager();
-    ScriptEngine engine = mgr.getEngineByExtension("vtl");
-    engine.put(VtlScriptEngine.PROCESSING_ENGINE_NAMES, "spark");
-
-    InMemoryDataset ds1 =
-        new InMemoryDataset(
-            List.of(
-                Map.of("id1", "A", "var1", 0L, "var2", 100L),
-                Map.of("id1", "B", "var1", 1L, "var2", 200L),
-                Map.of("id1", "C", "var1", 2L, "var2", 300L)),
-            Map.of("id1", String.class, "var1", Long.class, "var2", Long.class),
-            Map.of(
-                "id1",
-                Dataset.Role.IDENTIFIER,
-                "var1",
-                Dataset.Role.MEASURE,
-                "var2",
-                Dataset.Role.MEASURE));
-    InMemoryDataset ds2 =
-        new InMemoryDataset(
-            List.of(
-                Map.of("id1", "A", "var1", 10L, "var2", 1L),
-                Map.of("id1", "B", "var1", 11L, "var2", 2L),
-                Map.of("id1", "D", "var1", 12L, "var2", 3L)),
-            Map.of("id1", String.class, "var1", Long.class, "var2", Long.class),
-            Map.of(
-                "id1",
-                Dataset.Role.IDENTIFIER,
-                "var1",
-                Dataset.Role.MEASURE,
-                "var2",
-                Dataset.Role.MEASURE));
-
-    engine.put("ds1", ds1);
-    engine.put("ds2", ds2);
-
-    String script =
-        """
-                ds1 := ds1[calc identifier id1 := id1, var1 := cast(var1, integer), var2 := cast(var2, integer)];
-                ds2 := ds2[calc identifier id1 := id1, var1 := cast(var1, integer), var2 := cast(var2, integer)];
-                ds_sum := ds1 + ds2;
-                ds_mul := ds_sum * 3;\s
-                ds_res <- ds_mul[filter mod(var1, 2) = 0][calc var_sum := var1 + var2];\
-                """;
-
-    Program program =
-        ProvenanceListener.runWithBindings(
-            engine, script, "trevas-simple-test", "Simple test from Trevas tests");
-    Model model = RDFUtils.buildModel(program);
-    String content = RDFUtils.serialize(model, "JSON-LD");
-    assertThat(content).isNotEmpty();
-    RDFUtils.loadModelWithCredentials(
-        model, sparqlEndpoint, sparqlEndpointUser, sparlqEndpointPassword);
-    RDFUtils.writeJsonLdToFile(model, "src/test/resources/output/test-simple-with-bindings.json");
-    assertThat(program.getProgramSteps()).hasSize(5);
-  }
-
-  @Test
   public void bpeTest() throws IOException {
 
     String bpeScript =
         """
-                // Validation of municipality code in input file
-                CHECK_MUNICIPALITY := check_datapoint(BPE_DETAIL_VTL, UNIQUE_MUNICIPALITY invalid);
+                        // Define UNIQUE_MUNICIPALITY ruleset
+                        define datapoint ruleset UNIQUE_MUNICIPALITY (variable DEPCOM) is
+                          MUNICIPALITY_FORMAT_RULE : match_characters(DEPCOM, "[0-9]{5}|2[A-B][0-9]{3}") errorcode "Municipality code is not in the correct format"
+                        end datapoint ruleset;
 
-                // Clean BPE input database
-                BPE_DETAIL_CLEAN := BPE_DETAIL_VTL  [drop LAMBERT_X, LAMBERT_Y]
-                                            [rename ID_EQUIPEMENT to id, TYPEQU to facility_type, DEPCOM to municipality, REF_YEAR to year];
+                        // Validation of municipality code in input file
+                        CHECK_MUNICIPALITY := check_datapoint(BPE_DETAIL_VTL, UNIQUE_MUNICIPALITY invalid);
 
-                // BPE aggregation by municipality, type and year
-                BPE_MUNICIPALITY <- BPE_DETAIL_CLEAN    [aggr nb := count(id) group by municipality, year, facility_type];
+                        // Clean BPE input database
+                        BPE_DETAIL_CLEAN := BPE_DETAIL_VTL  [drop LAMBERT_X, LAMBERT_Y]
+                                                    [rename ID_EQUIPEMENT to id, TYPEQU to facility_type, DEPCOM to municipality, REF_YEAR to year];
 
-                // BPE aggregation by NUTS 3, type and year
-                BPE_NUTS3 <- BPE_MUNICIPALITY    [calc nuts3 := if substr(municipality,1,2) = "97" then substr(municipality,1,3) else substr(municipality,1,2)]
-                                                    [aggr nb := count(nb) group by year, nuts3, facility_type];
+                        // BPE aggregation by municipality, type and year
+                        BPE_MUNICIPALITY <- BPE_DETAIL_CLEAN    [aggr nb := count(id) group by municipality, year, facility_type];
 
-                // BPE validation of facility types by NUTS 3
-                CHECK_NUTS3_TYPES := check_datapoint(BPE_NUTS3, NUTS3_TYPES invalid);
+                        // BPE aggregation by NUTS 3, type and year
+                        BPE_NUTS3 <- BPE_MUNICIPALITY    [calc nuts3 := if substr(municipality,1,2) = "97" then substr(municipality,1,3) else substr(municipality,1,2)]
+                                                            [aggr nb := count(nb) group by year, nuts3, facility_type];
 
-                // Prepare 2021 census dataset by NUTS 3
-                CENSUS_NUTS3_2021 := LEGAL_POP   [rename REF_AREA to nuts3, TIME_PERIOD to year, POP_TOT to pop]
-                                                    [filter year = "2021"]
-                                                    [calc pop := cast(pop, integer)]
-                                                    [drop year, NB_COM, POP_MUNI];
+                        // Define ruleset
+                        define datapoint ruleset NUTS3_TYPES (variable facility_type, nb) is
+                         BOWLING_ALLEY_RULE : when facility_type = "F102" then nb > 10 errorcode "Not enough bowling alleys"
+                        end datapoint ruleset;
 
-                // Extract dataset on general practitioners from BPE by NUTS 3 in 2021
-                GENERAL_PRACT_NUTS3_2021 := BPE_NUTS3   [filter facility_type = "D201" and year = "2021"]
-                                            [drop facility_type, year];
+                        // BPE validation of facility types by NUTS 3
+                        CHECK_NUTS3_TYPES := check_datapoint(BPE_NUTS3, NUTS3_TYPES invalid);
 
-                // Merge practitioners and legal population datasets by NUTS 3 in 2021 and compute an indicator
-                BPE_CENSUS_NUTS3_2021 <- inner_join(GENERAL_PRACT_NUTS3_2021, CENSUS_NUTS3_2021)
-                                        [calc pract_per_10000_inhabitants := nb / pop * 10000]
-                                        [drop nb, pop];\
-                """;
+                        // Prepare 2021 census dataset by NUTS 3
+                        CENSUS_NUTS3_2021 := LEGAL_POP   [rename REF_AREA to nuts3, TIME_PERIOD to year, POP_TOT to pop]
+                                                            [filter year = "2021"]
+                                                            [calc pop := cast(pop, integer)]
+                                                            [drop year, NB_COM, POP_MUNI];
 
-    Program program = ProvenanceListener.run(bpeScript, "trevas-bpe-test", "BPE from Trevas tests");
+                        // Extract dataset on general practitioners from BPE by NUTS 3 in 2021
+                        GENERAL_PRACT_NUTS3_2021 := BPE_NUTS3   [filter facility_type = "D201" and year = "2021"]
+                                                    [drop facility_type, year];
+
+                        // Merge practitioners and legal population datasets by NUTS 3 in 2021 and compute an indicator
+                        BPE_CENSUS_NUTS3_2021 <- inner_join(GENERAL_PRACT_NUTS3_2021, CENSUS_NUTS3_2021)
+                                                [calc pract_per_10000_inhabitants := nb / pop * 10000]
+                                                [drop nb, pop];\
+                        """;
+    Structured.DataStructure bpeStructure =
+        TrevasSDMXUtils.buildStructureFromSDMX3(
+            "src/test/resources/input/DSD_BPE_CENSUS.xml", "BPE_DETAIL_VTL");
+    Structured.DataStructure censusStructure =
+        TrevasSDMXUtils.buildStructureFromSDMX3(
+            "src/test/resources/input/DSD_BPE_CENSUS.xml", "LEGAL_POP");
+
+    SparkDataset bpeDetailDs =
+        new SparkDataset(
+            spark
+                .read()
+                .option("header", "true")
+                .option("delimiter", ";")
+                .option("quote", "\"")
+                .csv("src/test/resources/input/BPE_DETAIL_SAMPLE.csv"),
+            bpeStructure);
+
+    SparkDataset censusNuts =
+        new SparkDataset(
+            spark
+                .read()
+                .option("header", "true")
+                .option("delimiter", ";")
+                .option("quote", "\"")
+                .csv("src/test/resources/input/LEGAL_POP_NUTS3.csv"),
+            censusStructure);
+
+    ScriptContext context = engine.getContext();
+    context.setAttribute("BPE_DETAIL_VTL", bpeDetailDs, ScriptContext.ENGINE_SCOPE);
+    context.setAttribute("LEGAL_POP", censusNuts, ScriptContext.ENGINE_SCOPE);
+
+    Program program =
+        ProvenanceListener.run(engine, bpeScript, "trevas-bpe-test", "BPE from Trevas tests");
     Model model = RDFUtils.buildModel(program);
     RDFUtils.loadModelWithCredentials(
         model, sparqlEndpoint, sparqlEndpointUser, sparlqEndpointPassword);
