f027d850e3cf7be5a082393b974a072cbc75854c
Nicolas Laval <nicolas.laval@making-sense.info>
2025-08-15 23:13:33 +0200

    Add AntlrUtils getDefineStatements

---
+++ vtl-prov/src/test/java/fr/insee/vtl/prov/utils/AntlrUtilsTest.java
@@ -0,0 +1,32 @@
+package fr.insee.vtl.prov.utils;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AntlrUtilsTest {
+
+  String simpleScript =
+      """
+                          ds_sum := ds1 + ds2;
+                          ds_mul := ds_sum * 3;\s
+                          ds_res <- ds_mul[filter mod(var1, 2) = 0][calc var_sum := var1 + var2];\
+                    """;
+
+  String simpleValidation =
+      """
+                          define datapoint ruleset test (variable sex) is
+                              myrule : sex in {"M"} errorcode "ERROR"
+                          end datapoint ruleset;
+                          pengfei.ds_result <- check_datapoint(pengfei.pengfei, test);
+                    """;
+
+  @Test
+  public void getDefineStatementsTest() {
+    assertThat(AntlrUtils.getDefineStatements(simpleScript).size()).isEqualTo(0);
+
+    assertThat(AntlrUtils.getDefineStatements(simpleValidation).size()).isEqualTo(1);
+    assertThat(AntlrUtils.getDefineStatements(simpleValidation).get("test"))
+        .contains("define datapoint ruleset test (variable sex) is");
+  }
+}
